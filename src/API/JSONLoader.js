2297a5c5bcd1240e034e2a022356eb0b49302f87
sasha240100 <siteprogcom@gmail.com>
2015-11-21 02:13:23 +0200

    Logos.

---
+++ src/API/JSONLoader.js
@@ -0,0 +1,10 @@
+/**
+ * © Alexander Buzin, 2014-2015
+ * Site: http://alexbuzin.me/
+ * Email: alexbuzin88@gmail.com
+*/
+
+// #DONE:0 JSONLoader don't work.
+WHS.API.JSONLoader = function() {
+  return new THREE.JSONLoader();
+}
