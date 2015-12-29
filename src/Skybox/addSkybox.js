540e1a0b9bae27623ea3d2c27ce21adb97f91c02
Jack Dalton <jack@jackdalton.org>
2015-12-29 09:21:55 -0700

    Use api.Wrap instead of scene.add

--- src/Skybox/addSkybox.js
+++ src/Skybox/addSkybox.js
@@ -2,41 +2,82 @@
  * © Alexander Buzin, 2014-2015
  * Site: http://alexbuzin.me/
  * Email: alexbuzin88@gmail.com
-*/
-
+ */
 /**
  * Adds a skybox to the WhitestormJS scene.
  * @param {Object} options - Skybox options.
  * @param {String} options.src - Skybox image source.
  * @param {String} options.imgSuffix - Skybox image suffix (.png, .jpg, etc.)
+ * @param {String} options.skyType - Type of sky. Either box or dome.
  * @returns {Object} scope - Scope.
  */
 WHS.init.prototype.addSkybox = function(options) {
   'use strict';
 
-  options.imgSuffix = options.imgSuffix || ".png";
+  options.skyType = options.skyType || "box";
+  options.imgSuffix = options.skyType == "box" ? options.imgSuffix || ".png" : options.imgSuffix || "";
   options.onlyvis = true;
+
   var scope = new api.construct(this, options, "skybox");
 
-  var axes = new THREE.AxisHelper(100);
-  var scene = this.scene;
-  var directions = ["xpos", "xneg", "ypos", "yneg", "zpos", "zneg"];
-  var skyGeometry = new THREE.CubeGeometry(5000, 5000, 5000);
-  var matArray = [];
+  switch (options.skyType) {
+    case "box":
+      var directions = ["xpos", "xneg", "ypos", "yneg", "zpos", "zneg"];
+      var skyGeometry = new THREE.CubeGeometry(5000, 5000, 5000);
+      var matArray = [];
 
-  scene.add(axes);
+      for (var i = 0; i < 6; i++) {
+        matArray.push(new THREE.MeshBasicMaterial({
+          map: THREE.ImageUtils.loadTexture(options.src + directions[i] + options.imgSuffix),
+          side: THREE.BackSide
+        }));
+      }
 
-  for (var i = 0; i < 6; i++) {
-    matArray.push(new THREE.MeshBasicMaterial({
-      map: THREE.ImageUtils.loadTexture(options.src + directions[i] + options.imgSuffix),
-      side: THREE.BackSide
-    }));
+      var skyMat = new THREE.MeshFaceMaterial(matArray);
+      var skybox = new THREE.Mesh(skyGeometry, skyMat);
+
+      api.Wrap(scope, skybox, null);
+
+      break;
+    case "dome":
+      var vertexShader = [
+        "varying vec2 vUV;",
+        "",
+        "void main() {",
+        "vUV = uv;",
+        "vec4 pos = vec4(position, 1.0);",
+        "gl_Position = projectionMatrix * modelViewMatrix * pos;",
+        "}"
+      ].join("\n");
+      var fragmentShader = [
+        "uniform sampler2D texture;",
+        "varying vec2 vUV;",
+        "",
+        "void main() {",
+        "vec4 sample = texture2D(texture, vUV);",
+        "gl_FragColor = vec4(sample.xyz, sample.w);",
+        "}"
+      ].join("\n");
+      var skyGeometry = new THREE.SphereGeometry(3000, 60, 40);
+      var skyMaterial = new THREE.ShaderMaterial({
+        uniforms: {
+          texture: {
+            type: "t",
+            value: THREE.ImageUtils.loadTexture(options.src + options.imgSuffix)
+          }
+        },
+        vertexShader: vertexShader,
+        fragmentShader: fragmentShader
+      });
+      var skybox = new THREE.Mesh(skyGeometry, skyMaterial);
+
+      skybox.scale.set(-1, 1, 1);
+      skybox.rotation.order = "XZY";
+      skybox.renderDepth = 1000.0;
+
+      api.Wrap(scope, skybox, null);
+
+      break;
   }
-
-  var skyMat = new THREE.MeshFaceMaterial(matArray);
-  var skybox = new THREE.Mesh(skyGeometry, skyMat);
-
-  scene.add(skybox);
-
   return scope;
 };
