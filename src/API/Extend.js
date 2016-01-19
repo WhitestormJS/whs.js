12c3582c30760a88f0c0def320eb798cbb361899
sasha240100 <siteprogcom@gmail.com>
2016-01-19 16:19:50 +0200

    CSF. Extend.js #25
    Former-commit-id: e61df0b086c14db31bef0a2836754456488d38a9

--- src/API/Extend.js
+++ src/API/Extend.js
@@ -4,13 +4,15 @@
  * Email: alexbuzin88@gmail.com
 */
 
-WHS.API.extend = function(object, ...extensions){ // $.extend alternative, ... is the spread operator
-	for(var extension of extensions){
-		if(!extension)
-			continue; //Ignore null and undefined objects and paramaters
-		for(var prop of Object.getOwnPropertyNames(extension)){ //Do not traverse the prototype chain
-				object[prop] = (object[prop] === 0)? 0 : object[prop] || extension[prop]; //Add values that do not already exist
+WHS.API.extend = function( object, ...extensions ) { // $.extend alternative, ... is the spread operator.
+	for( var extension of extensions ){
+		if( !extension )
+			continue; // Ignore null and undefined objects and paramaters.
+
+		for(var prop of Object.getOwnPropertyNames(extension)){ // Do not traverse the prototype chain.
+				object[prop] = (object[prop] === 0)? 0 : object[prop] || extension[prop]; // Add values that do not already exist.
 		}
 	}
+
 	return object;
 }
