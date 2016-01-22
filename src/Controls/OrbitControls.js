9130741b72b90c37647ac1ff3ce65b9e4d154eb0
sasha240100 <siteprogcom@gmail.com>
2016-01-22 14:42:20 +0200

    Fix. OrbitControls #25

--- src/Controls/OrbitControls.js
+++ src/Controls/OrbitControls.js
@@ -10,6 +10,17 @@
  * @param {Object} object Description. (OPTIONAL)
  */
 WHS.init.prototype.OrbitControls = function(object) {
-  // #TODO:170 add use for object.
-  this.controls = new THREE.OrbitControls(this._camera, this.renderer.domElement);
+
+	this.controls = new THREE.OrbitControls(this._camera, this.renderer.domElement);
+
+	if (object._whsobject) {
+		
+		var target = object ? object.visible.position : new THREE.Vector3( 0, 0, 0 );
+		this.controls.target = target;
+
+	} else if (typeof object == "object")
+		this.controls.target.copy(target);
+	else
+		console.error("Object must be a THREE.JS vector! @OrbitControls");
+
 }
