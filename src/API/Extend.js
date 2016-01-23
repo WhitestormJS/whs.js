725a200b44a3dcbaaa88198141b6b2351cf2b2e9
Noctisdark <diseven@outlook.fr>
2016-01-23 22:05:03 +0100

    Makes extend method deep
    Extend is deep only if object[prop] and extension[prop] are both objects !

--- src/API/Extend.js
+++ src/API/Extend.js
@@ -10,7 +10,12 @@ WHS.API.extend = function( object, ...extensions ) { // $.extend alternative, ..
 			continue; // Ignore null and undefined objects and paramaters.
 
 		for( var prop of Object.getOwnPropertyNames( extension ) ) { // Do not traverse the prototype chain.
-			object[prop] = ( object[prop] === 0 )? 0 : object[prop] || extension[prop]; // Add values that do not already exist.
+			if(object[prop] != undefined && object[prop].toString() == '[object Object]' && extension[prop].toString() == '[object Object]')
+				//Goes deep only if object[prop] and extension[prop] are both objects !
+				WHS.API.extend(object[prop], extension[prop]);
+					
+			else
+				object[prop] = ( object[prop] === 0 )? 0 : object[prop] || extension[prop]; // Add values that do not already exist.
 		}
 	}
 
