6ed93ed608498d3c8fde75f576036f46bef2642d
sasha240100 <siteprogcom@gmail.com>
2016-02-04 19:34:37 +0200

    api.construct -> WHS.Shape #48
    Former-commit-id: 518f47473f7fbadf62311e5dfec7c9b1616021f1

--- gulpfile.js
+++ gulpfile.js
@@ -9,72 +9,72 @@ var sourcemaps = require("gulp-sourcemaps");
 var babel = require("gulp-babel");
 
 var sources = [
-  'src/libs/*.js',
-  'src/libs/**/*.js',
-  'src/Polyfill.js',
-  'src/Prefix.js',
-  'src/Shaders/*.js',
-  'src/API/*.js',
-  'src/Watchers/*.js',
-  'src/Plugins/*.js',
-  'src/Core.js',
-  'src/Objects/*.js',
-  'src/Ground/*.js',
-  'src/Fog/*.js',
-  'src/Light/*.js',
-  'src/Wagner/*.js',
-  'src/Controls/*.js',
-  'src/GUI/*.js',
-  'src/Skybox/*.js'
+    'src/libs/*.js',
+    'src/libs/**/*.js',
+    'src/Polyfill.js',
+    'src/Prefix.js',
+    'src/Shaders/*.js',
+    'src/API/*.js',
+    'src/Watchers/*.js',
+    'src/Plugins/*.js',
+    'src/Core.js',
+    'src/Objects/*.js',
+    'src/Ground/*.js',
+    'src/Fog/*.js',
+    'src/Light/*.js',
+    'src/Wagner/*.js',
+    'src/Controls/*.js',
+    'src/GUI/*.js',
+    'src/Skybox/*.js'
 ]
 
 var sources_test = [
-  'src/libs/*.js',
-  'src/libs/**/*.js',
-  'src/Prefix.js',
-  'src/Shaders/*.js',
-  'src/API/*.js',
-  'src/Watchers/*.js',
-  'src/Plugins/*.js',
-  'src/Core.js',
-  'src/Objects/*.js',
-  'src/Ground/*.js',
-  'src/Fog/*.js',
-  'src/Light/*.js',
-  'src/Wagner/*.js',
-  'src/Controls/*.js',
-  'src/GUI/*.js',
-  'src/Skybox/*.js'
+    'src/libs/*.js',
+    'src/libs/**/*.js',
+    'src/Prefix.js',
+    'src/Shaders/*.js',
+    'src/API/*.js',
+    'src/Watchers/*.js',
+    'src/Plugins/*.js',
+    'src/Core.js',
+    'src/Objects/*.js',
+    'src/Ground/*.js',
+    'src/Fog/*.js',
+    'src/Light/*.js',
+    'src/Wagner/*.js',
+    'src/Controls/*.js',
+    'src/GUI/*.js',
+    'src/Skybox/*.js'
 ]
 
 var codes = [
-  'src/Prefix.js',
-  'src/Shaders/*.js',
-  'src/API/*.js',
-  'src/Watchers/*.js',
-  'src/Core.js',
-  'src/Objects/*.js',
-  'src/Ground/*.js',
-  'src/Fog/*.js',
-  'src/Light/*.js',
-  'src/Wagner/*.js',
-  'src/Controls/*.js',
-  'src/GUI/*.js',
-  'src/Skybox/*.js'
+    'src/Prefix.js',
+    'src/Shaders/*.js',
+    'src/API/*.js',
+    'src/Watchers/*.js',
+    'src/Core.js',
+    'src/Objects/*.js',
+    'src/Ground/*.js',
+    'src/Fog/*.js',
+    'src/Light/*.js',
+    'src/Wagner/*.js',
+    'src/Controls/*.js',
+    'src/GUI/*.js',
+    'src/Skybox/*.js'
 ]
 
 
 
 var author_comment = "/**\n" +
-  " * © Alexander Buzin, 2014-2015\n" +
-  " * Site: http://alexbuzin.me/\n" +
-  " * Email: alexbuzin88@gmail.com\n" +
-  "*/\n" +
-  "\n";
+    " * © Alexander Buzin, 2014-2015\n" +
+    " * Site: http://alexbuzin.me/\n" +
+    " * Email: alexbuzin88@gmail.com\n" +
+    "*/\n" +
+    "\n";
 
 var lib_includes = [
-  "var THREE = require('three');",
-  "var jQuery = require('jquery');"
+    "var THREE = require('three');",
+    "var jQuery = require('jquery');"
 ].join("\n");
 
 
@@ -83,75 +83,78 @@ var lib_includes = [
 // Build command.
 gulp.task('build', function() {
 
-  gulp.src(sources)
-    .pipe(replace({
-      patterns: [
-        {
-          match: new RegExp(
-            '(\\/)(.)(.)(\\s+)(.)(\\s+)(.)(\\s+)' +
-            '((?:[a-z][a-z]+))(\\s+)((?:[a-z][a-z]+))(.)' +
-            '(\\s+)(\\d+)([-+]\\d+)(\\s+)(.)(\\s+)' +
-            '((?:[a-z][a-z]+))(.)(\\s+)' +
-            '((?:http|https)(?::\\/{2}[\\w]+)(?:[\\/|\\.]?)(?:[^\\s"]*))' +
-            '(\\s+)(.)(\\s+)((?:[a-z][a-z]+))(.)(\\s+)' +
-            '([\\w-+]+(?:\\.[\\w-+]+)*@(?:[\\w-]+\\.)+[a-zA-Z]{2,7})' +
-            '(\\s+)(.)(.)'
-            ,["i"]),
-          replacement: ''
-        }
-      ]
-    }))
-    .pipe(sourcemaps.init())
-    .pipe(concat('whitestorm.js'))
-    .pipe(babel({
+    gulp.src(sources)
+        .pipe(replace({
+            patterns: [
+                {
+                    match: new RegExp(
+                        '(\\/)(.)(.)(\\s+)(.)(\\s+)(.)(\\s+)' +
+                        '((?:[a-z][a-z]+))(\\s+)((?:[a-z][a-z]+))(.)' +
+                        '(\\s+)(\\d+)([-+]\\d+)(\\s+)(.)(\\s+)' +
+                        '((?:[a-z][a-z]+))(.)(\\s+)' +
+                        '((?:http|https)(?::\\/{2}[\\w]+)(?:[\\/|\\.]?)(?:[^\\s"]*))' +
+                        '(\\s+)(.)(\\s+)((?:[a-z][a-z]+))(.)(\\s+)' +
+                        '([\\w-+]+(?:\\.[\\w-+]+)*@(?:[\\w-]+\\.)+[a-zA-Z]{2,7})' +
+                        '(\\s+)(.)(.)'
+                        ,["i"]),
+                    replacement: ''
+                }
+            ]
+        }))
+        .pipe(sourcemaps.init())
+        .pipe(concat('whitestorm.js'))
+        .pipe(babel({
             presets: ['es2015']
         }))
-    .pipe(beautify())
-    .pipe(insert.prepend(author_comment))
-    .pipe(gulp.dest('./build/'));
+        .pipe(beautify())
+        .pipe(insert.prepend(author_comment))
+        .pipe(gulp.dest('./build/'));
 
-  gulp.src(sources_test)
-    .pipe(concat("whitestorm.test.js"))
-    .pipe(babel({
+    gulp.src(sources_test)
+        .pipe(concat("whitestorm.test.js"))
+        .pipe(babel({
             presets: ['es2015']
         }))
-    .pipe(insert.prepend(lib_includes))
-    .pipe(uglify())
-    .pipe(insert.prepend(author_comment))
-    .pipe(gulp.dest("./build/"));
+        .pipe(insert.prepend(lib_includes))
+        .pipe(uglify())
+        .pipe(insert.prepend(author_comment))
+        .pipe(gulp.dest("./build/"));
 
-  gulp.src(sources)
-    .pipe(concat('whitestorm.min.js'))
-    .pipe(babel({
+    gulp.src(sources)
+        .pipe(concat('whitestorm.min.js'))
+        .pipe(babel({
             presets: ['es2015']
         }))
-    .pipe(uglify())
-    .pipe(insert.prepend(author_comment))
-    .pipe(gulp.dest('./build/'));
+        .pipe(uglify())
+        .pipe(insert.prepend(author_comment))
+        .pipe(gulp.dest('./build/'));
 
 });
 
 // Test command.
 gulp.task('test', function() {
 
-  gulp.src(sources)
-    .pipe(concat('whitestorm.js'))
-    .pipe(gulp.dest('./build/'));
+    gulp.src(sources)
+        .pipe(concat('whitestorm.js'))
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('./build/'));
 
 });
 
 // Watch command.
 gulp.task('watch', function() {
 
-  gulp.watch(sources, ['test']);
+    gulp.watch(sources, ['test']);
 
-  watch('src/**/*.js', {
-    events: ['add']
-  }, function(file) {
+    watch('src/**/*.js', {
+        events: ['add']
+    }, function(file) {
 
     gulp.src(file.path).pipe(insert.prepend(author_comment))
-      .pipe(gulp.dest(file.dirname));
-      
-  });
+        .pipe(gulp.dest(file.dirname));
+            
+    });
 
 });
---
+++ src/API/Shape.js
@@ -0,0 +1,197 @@
+/**
+ * © Alexander Buzin, 2014-2015
+ * Site: http://alexbuzin.me/
+ * Email: alexbuzin88@gmail.com
+*/
+
+/**
+ * © Alexander Buzin, 2014-2015
+ * Site: http://alexbuzin.me/
+ * Email: alexbuzin88@gmail.com
+*/
+
+//WHS.API.construct = function( root, params, type ) {
+
+
+WHS.Shape = class {
+
+	constructor( root, params, type ) {
+
+		if ( ! root )
+		console.error( "@constructor: WHS root object is not defined." );
+
+		var _set = function( x, y, z ) {
+
+			this.x = x;
+			this.y = y;
+			this.z = z;
+
+		}
+
+		if ( params.pos ) params.pos.set = _set;
+		if ( params.rot ) params.rot.set = _set;
+		if ( params.scale ) params.scale.set = _set;
+		if ( params.target ) params.target.set = _set;
+
+		// Polyfill for 3D.
+		var target = api.extend(params, {
+
+			pos: {
+				x: 0,
+				y: 0,
+				z: 0,
+				set: _set
+			},
+
+			rot: {
+				x: 0,
+				y: 0,
+				z: 0,
+				set: _set
+			},
+
+			scale: {
+				x: 1,
+				y: 1,
+				z: 1,
+				set: _set
+			},
+
+			target: {
+				x: 0,
+				y: 0,
+				z: 0,
+				set: _set
+			},
+
+			morph: {
+				speed: 1,
+				duration: 1
+			},
+
+			onlyvis: false
+
+		} );
+
+
+		var key = 0;
+
+		root.modellingQueue.forEach( function( el ) {
+
+			if ( el.type == type ) key ++;
+
+		} );
+
+		var scope = {
+			root: root,
+			_key: key,
+			_whsobject: true,
+			_name: type + key,
+			__releaseTime: new Date().getTime(),
+			_pos: target.pos,
+			_rot: target.rot,
+			_scale: target.scale,
+			_morph: target.morph,
+			_target: target.target,
+			_onlyvis: target.onlyvis
+		};
+
+		Object.assign( this, scope );
+
+		return this;
+	}
+
+
+	build( mesh ) {
+		
+		'use strict';
+
+		mesh = mesh || this.mesh;
+
+		this.build_state = new Promise( (resolve, reject) => {
+
+			try {
+
+				mesh.castShadow = true;
+				mesh.receiveShadow = true;
+
+				mesh.position.set( this._pos.x, this._pos.y, this._pos.z );
+				mesh.rotation.set( this._rot.x, this._rot.y, this._rot.z );
+				mesh.scale.set( this._scale.x, this._scale.y, this._scale.z );
+
+				resolve();
+
+			} catch ( err ) {
+
+				console.error( err.message );
+
+				reject();
+
+				//this._state.reject();
+
+			}
+
+		});
+
+		return this;
+	}
+
+
+	wrap( mesh ) {
+
+		'use strict';
+
+		var _mesh = mesh || this.mesh,
+			_scope = this;
+
+		this._key = this.root.modellingQueue.length;
+
+		this._state = new Promise( (resolve, reject) => {
+
+			try {
+
+				api.merge( _scope.root.scene, _mesh );
+				_scope.root.modellingQueue.push( _scope );
+
+			} catch ( err ) {
+
+				console.error( err.message );
+				reject();
+
+			} finally {
+
+				if ( _scope._wait ) {
+
+					_scope._mesh.addEventListener( 'ready', function() {
+						resolve();
+					} );
+
+				} else
+					resolve();
+
+			}
+
+		} );
+
+		_scope.root.children.push( _scope );
+
+		return this;
+	}
+
+	remove() {
+		
+		this.root.scene.remove( this.mesh );
+
+		return this;
+
+	}
+
+	retrieve() {
+
+		this.root.scene.add( this.mesh );
+
+		return this;
+
+	}
+
+}
