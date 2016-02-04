4b2afe2be6b4cb726832303e62f467083104a7da
sasha240100 <siteprogcom@gmail.com>
2016-02-04 18:35:47 +0200

    Defaults for api.texture() #25

--- build/whitestorm.js
+++ build/whitestorm.js
@@ -2146,27 +2146,23 @@ WHS.API.texture = function(url, options) {
 
     if (options) {
 
-        var opt = options;
+        var opt = api.extend(options, {
 
-        opt.offset = opt.offset || {
-            x: 1,
-            y: 1
-        };
+            offset: {
+                x: 0,
+                y: 0
+            },
 
-        opt.offset.x = opt.offset.x || 1;
-        opt.offset.y = opt.offset.y || 1;
+            repeat: {
+                x: 1,
+                y: 1
+            }
 
-        opt.repeat = opt.repeat || {
-            x: 1,
-            y: 1
-        };
-
-        opt.repeat.x = opt.repeat.x || 1;
-        opt.repeat.y = opt.repeat.y || 1;
+        });
 
         texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
 
-        //texture.offset.set(opt.offset.x, opt.offset.y);
+        texture.offset.set(opt.offset.x, opt.offset.y);
         texture.repeat.set(opt.repeat.x, opt.repeat.y);
 
         texture.magFilter = THREE.NearestFilter;
