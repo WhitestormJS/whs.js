60962208b37e1b14d5c6666ffc19b17a453cfdbd
Jack Dalton <jack@jackdalton.org>
2016-03-21 11:23:45 -0600

    Update docs #63

--- src/Shapes/Icosahedron.js
+++ src/Shapes/Icosahedron.js
@@ -4,7 +4,24 @@
  * Email: alexbuzin88@gmail.com
 */
 
+/**
+ * WhitestormJS icosahedron shape.
+ *
+ * @extends WHS.Shape
+ */
+
 WHS.Icosahderon = class Icosahedron extends WHS.Shape {
+
+    /**
+     * Create an icosahedron
+     *
+     * @param {Object} params - Icosahedron options
+     * @param {Object} params.geometry - Icosahedron geometry options
+     * @param {Number} params.geometry.radius - Icosahedron radius
+     * @param {Number} params.geometry.detail - Icosahedron detail
+     * @param {Material} params.material - Icosahedron material
+     * @param {Number} params.mass - Icosahedron mass
+     */
 
 	constructor( params ) {
 
@@ -17,16 +34,16 @@ WHS.Icosahderon = class Icosahedron extends WHS.Shape {
 
         });
 
-		this.mesh = new Physijs.ConvexMesh( 
+		this.mesh = new Physijs.ConvexMesh(
             new THREE.IcosahedronGeometry(
 
                 params.geometry.radius,
                 params.geometry.detail
 
-            ), 
+            ),
 
-            super._initMaterial(params.material), 
-            params.mass 
+            super._initMaterial(params.material),
+            params.mass
         );
 
         super.build();
