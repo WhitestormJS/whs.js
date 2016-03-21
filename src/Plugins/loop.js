6a3d61c89aee71a7b8312eba5ee261abbf18f888
sasha240100 <siteprogcom@gmail.com>
2016-03-21 23:36:18 +0200

    ...

---
+++ src/Plugins/loop.js
@@ -0,0 +1,40 @@
+/**
+ * © Alexander Buzin, 2014-2015
+ * Site: http://alexbuzin.me/
+ * Email: alexbuzin88@gmail.com
+*/
+
+/**
+ * WhitestormJS plugin loop
+ *
+ * @param  {Function} func - Function to be executed
+ */
+WHS.loop = function( func ) {
+
+    this.loop = {
+        func: func,
+        id: WHS.loops.length,
+        enabled: false
+    };
+
+    WHS.loops.push( this.loop );
+
+}
+
+/**
+ * Starts the loop
+ */
+WHS.loop.prototype.start = function() {
+
+    this.loop.enabled = true;
+
+};
+
+/**
+ * Stops the loop
+ */
+WHS.loop.prototype.stop = function() {
+
+    this.loop.enabled = false;
+
+};
