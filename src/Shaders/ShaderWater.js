988f28d44256d7c7c356c2bed3adcaec8c3be809
Estuardo Diaz <esjoando555@gmail.com>
2016-03-25 13:34:49 -0600

    WIP added new gif and removed unnecesary styles
    Former-commit-id: 03088a8634669f82789bc6b6a6807426359fc248

---
+++ src/Shaders/ShaderWater.js
@@ -0,0 +1,16 @@
+/**
+ * © Alexander Buzin, 2014-2015
+ * Site: http://alexbuzin.me/
+ * Email: alexbuzin88@gmail.com
+*/
+
+/**
+* @author t4r0 / http://t4r0.me
+*
+*/
+
+THREE.ShaderWater = {
+  'water' : {
+    
+  }
+};
