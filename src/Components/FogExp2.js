839c342a199d5ee725ddbb614dc4c8aaa95dbe0b
sasha240100 <siteprogcom@gmail.com>
2016-03-27 10:13:39 +0300

    WIP.
    Former-commit-id: 2050b32efc95625f56e92f50df9d09e7ec8dc953

---
+++ src/Components/FogExp2.js
@@ -0,0 +1,32 @@
+/**
+ * © Alexander Buzin, 2014-2015
+ * Site: http://alexbuzin.me/
+ * Email: alexbuzin88@gmail.com
+*/
+
+WHS.FogExp2 = class FogExp2 {
+
+	constructor( params ) {
+
+        api.extend(params, {
+
+            hex: 0x000000,
+            density: 0.00025
+
+        });
+
+        this.fog = new THREE.FogExp2( params.hex, params.density);
+
+	}
+
+    addTo( root ) {
+        
+        root.scene.fog = this.fog;
+
+    }
+
+}
+
+WHS.World.prototype.FogExp2 = function( params ) {
+	return ( new WHS.FogExp2(  params ) ).addTo( this );
+}
