c716a246e3d8824c0240b35cefa42acc549c48f7
Plusb Preco <plusb21@gmail.com>
2016-05-19 23:49:21 +0900

    Add build system
    [ci skip]
    
    
    Former-commit-id: 719ca2db8581a65d4356a3e45993acf695d8856b

---
+++ webpack.config.babel.js
@@ -0,0 +1,40 @@
+import {join} from 'path';
+import webpack from 'webpack';
+
+process.env.BABEL_ENV = 'browser';
+
+function config(production) {
+  return {
+    devtool: production ? 'hidden-source-map' : 'eval-source-map',
+    entry: './src/index.js',
+    output: {
+      path: join(__dirname, 'build'),
+      library: 'WHS',
+      libraryTarget: 'umd',
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel',
+        },
+      ],
+    },
+    plugins: production
+      ? [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+          mangle: false,
+          compress: {
+            warnings: false,
+          },
+        }),
+      ]
+      : [],
+  };
+}
+
+export {
+  config as default,
+};
