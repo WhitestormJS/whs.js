c8d00320cf2420e0caa224da016ebdd8bebbc2f4
Plusb Preco <plusb21@gmail.com>
2016-05-22 19:02:29 +0900

    Small fixes
    [ci skip]
    
    
    Former-commit-id: ae7be50e3ac3685b8c48c4f2bf335242bd27aef5

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,38 +3,38 @@ import webpack from 'webpack';
 
 process.env.BABEL_ENV = 'browser';
 
-function config(production) {
-  return {
-    devtool: production ? 'hidden-source-map' : 'eval-source-map',
-    entry: './src/index.js',
-    output: {
-      path: join(__dirname, 'dist'),
-      library: 'WHS',
-      libraryTarget: 'umd',
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: 'babel',
+function config({production}) {
+    return {
+        devtool: production ? 'hidden-source-map' : 'eval-source-map',
+        entry: './src/index.js',
+        output: {
+            path: join(__dirname, 'dist'),
+            library: 'WHS',
+            libraryTarget: 'umd'
         },
-      ],
-    },
-    plugins: production
-      ? [
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.UglifyJsPlugin({
-          mangle: false,
-          compress: {
-            warnings: false,
-          },
-        }),
-      ]
-      : [],
-  };
+        module: {
+            loaders: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: 'babel'
+                }
+            ]
+        },
+        plugins: production ?
+            [
+                new webpack.optimize.DedupePlugin(),
+                new webpack.optimize.UglifyJsPlugin({
+                    mangle: false,
+                    compress: {
+                        warnings: false
+                    }
+                })
+            ] :
+            []
+    };
 }
 
 export {
-  config as default,
+    config as default
 };
