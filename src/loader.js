b65e102acb43f2b7a61b920ebd3bc4d337a9c88e
yeliex <yeliex@yeliex.com>
2016-05-24 15:13:58 +0800

    Update loader to module
    Former-commit-id: b0cee48392c2e666578ef037158532975354ba1f

--- src/loader.js
+++ src/loader.js
@@ -2,22 +2,17 @@
  * © Alexander Buzin, 2014-2015
  * Site: http://alexbuzin.me/
  * Email: alexbuzin88@gmail.com
-*/
+ */
 
-WHS.API.loadJSON = function (url, callback, texturePath) {
-    let loader = new THREE.JSONLoader();
+import THREE from "three";
 
-    return loader.JSON.load(url, callback, texturePath);
-};
+const
+  loadJson = (new THREE.JSONLoader()).load,
+  loadTexture = (new THREE.TextureLoader()).load,
+  loadFont = (new THREE.FontLoader()).load;
 
-WHS.API.loadTexture = function (url, onLoad, onProgress, onError) {
-    let loader = new THREE.TextureLoader();
-
-    return loader.Texture.load(url, onLoad, onProgress, onError);
-};
-
-WHS.API.loadFont = function (url, onLoad, onProgress, onError) {
-    let loader = new THREE.FontLoader();
-
-    return loader.Font.load(url, onLoad, onProgress, onError);
+export {
+  loadJson,
+  loadTexture,
+  loadFont
 };
