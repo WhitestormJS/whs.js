4daf612eaa743e949249a39e9b0688e4cfb6d9e9
Plusb Preco <plusb21@gmail.com>
2016-05-26 02:54:59 +0900

    Update directory structure
    Former-commit-id: ba0f2f0a14b3e835386ca1f123d98a9cf2708c77

--- src/utils/Watch.js
+++ src/utils/Watch.js
@@ -8,14 +8,14 @@ class Watch {
   constructor(queue) {
     this._queue = Array.isArray(queue) ? queue.slice() : [];
   }
-  
+
   add(element) {
     this._queue.push(element);
   }
-  
+
   remove(element) {
     this._queue = this._queue.filter(function (item) {
-      return item != element;
+      return item !== element;
     });
   }
 }
