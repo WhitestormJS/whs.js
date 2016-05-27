b20b3ae6f7b31ccf14f8513c41ded22117ea6db5
Plusb Preco <plusb21@gmail.com>
2016-05-28 05:24:38 +0900

    Add main index files

---
+++ src/lights/index.js
@@ -0,0 +1,6 @@
+export AmbientLight from './AmbientLight';
+export DirectionalLight from './DirectionalLight';
+export HemisphereLight from './HemisphereLight';
+export NormalLight from './NormalLight';
+export PointLight from './PointLight';
+export SpotLight from './SpotLight';
---
+++ src/scenes/index.js
@@ -0,0 +1,4 @@
+export Fog from './Fog';
+export FogExp2 from './FogExp2';
+export Group from './Group';
+export Skybox from './Skybox';
