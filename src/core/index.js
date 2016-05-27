fc7ad16675d0a5ea2eebe892523fed03681f1315
Plusb Preco <plusb21@gmail.com>
2016-05-28 05:24:38 +0900

    Add main index files
    Former-commit-id: b20b3ae6f7b31ccf14f8513c41ded22117ea6db5

---
+++ src/core/index.js
@@ -0,0 +1,6 @@
+export Camera from './Camera';
+export Curve from './Curve';
+export Light from './Light';
+export Object from './Object';
+export Shape from './Shape';
+export World from './World';
---
+++ src/extensions/index.js
@@ -0,0 +1,1 @@
+export Loop from './Loop';
