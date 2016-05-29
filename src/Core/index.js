3f79c273ad81d94c11753fcfea622d1d23c4a2aa
sasha240100 <siteprogcom@gmail.com>
2016-05-30 01:19:17 +0300

    Remove duplicated directory

--- src/Core/index.js
+++
@@ -1,6 +0,0 @@
-export Camera from './camera';
-export Curve from './curve';
-export Light from './light';
-export Object from './object';
-export Shape from './shape';
-export World from './world';
