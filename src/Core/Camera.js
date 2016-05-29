e1292cbb50a41a1772914deaa8d49d06090b28ee
sasha240100 <siteprogcom@gmail.com>
2016-05-30 00:51:09 +0300

    CSF. camera.js
    Former-commit-id: 3c2b91f593c74311b6e5be2fe01d1aaf4d273c3f

--- src/Core/Camera.js
+++ src/Core/Camera.js
@@ -6,6 +6,7 @@
 
 /** Camera super class */
 WHS.Camera = class extends WHS.Object {
+
   /**
    * Constructing WHS.Camera object.
    *
@@ -14,14 +15,15 @@ WHS.Camera = class extends WHS.Object {
    * @return {WHS.Camera}
    */
   constructor(params, type) {
-    if (!type) {
-      console.error('@constructor: Please specify " type ".');
-    }
 
-    let _set = function (x, y, z) {
+    if (!type) console.error('@constructor: Please specify " type ".');
+
+    const _set = function (x, y, z) {
+
       this.x = x;
       this.y = y;
       this.z = z;
+
     };
 
     params.useTarget = Boolean(params.target);
@@ -67,7 +69,7 @@ WHS.Camera = class extends WHS.Object {
 
     super.setParams(params);
 
-    var scope = Object.assign(
+    const scope = Object.assign(
       this,
       {
         _type: type,
@@ -75,12 +77,11 @@ WHS.Camera = class extends WHS.Object {
       }
     );
 
-    if (WHS.debug) {
-      console.debug('@WHS.Camera: Camera ' + scope._type +
-      ' found.', scope);
-    }
+    if (WHS.debug)
+      console.debug(`@WHS.Camera: Camera ${scope._type} found.`, scope);
 
     return scope;
+
   }
 
   /**
@@ -90,12 +91,13 @@ WHS.Camera = class extends WHS.Object {
    * additionally.
    */
   wrap(...tags) {
-    'use strict';
 
-    let _scope = this;
+    const _scope = this;
 
     return new Promise((resolve, reject) => {
+
       try {
+
         _scope.position.set(
           _scope.__params.pos.x,
           _scope.__params.pos.y,
@@ -108,33 +110,38 @@ WHS.Camera = class extends WHS.Object {
           _scope.__params.rot.z
         );
 
-        if (_scope.__params.useTarget) {
-          _scope.lookAt(_scope.__params.target);
-        }
+        if (_scope.__params.useTarget) _scope.lookAt(_scope.__params.target);
 
         if (_scope.__params.helper) {
+
           _scope.helper = new THREE.CameraHelper(
             _scope.getNative()
           );
+
         }
 
         tags.forEach(tag => {
+
           _scope[tag] = true;
+
         });
 
-        if (WHS.debug) {
-          console.debug('@WHS.Camera: Camera ' + _scope._type + ' is ready.', _scope);
-        }
+        if (WHS.debug)
+          console.debug(`@WHS.Camera: Camera ${_scope._type} is ready.`, _scope);
 
         _scope.emit('ready');
 
         resolve(_scope);
-      } catch (err) {
-        console.error(err.message);
 
+      } catch (err) {
+
+        console.error(err.message);
         reject();
+
       }
+
     });
+
   }
 
   /**
@@ -144,44 +151,54 @@ WHS.Camera = class extends WHS.Object {
    * @param {...String} tags - Tags for compiling.
    */
   addTo(parent) {
-    'use strict';
 
     this.parent = parent;
 
-    let _helper = this.helper;
-    let _scope = this;
+    const _helper = this.helper,
+      _scope = this;
 
     return new Promise((resolve, reject) => {
+
       try {
+
         _scope.parent.getScene().add(_scope.getNative());
         _scope.parent.children.push(_scope);
 
-        if (_helper) {
-          _scope.parent.getScene().add(_helper);
-        }
+        if (_helper) _scope.parent.getScene().add(_helper);
+
       } catch (err) {
+
         console.error(err.message);
         reject();
+
       } finally {
+
         if (WHS.debug) {
+
           console.debug(
-            '@WHS.Camera: Camera ' + _scope._type + ' was added to world.',
+            `@WHS.Camera: Camera ${_scope._type} was added to world.`,
             [_scope, _scope.parent]
           );
+
         }
 
         resolve(_scope);
 
         _scope.emit('ready');
+
       }
+
     });
+
   }
 
   /**
    * Clone camera.
    */
   clone() {
+
     return new WHS.Shape(this.__params, this._type).copy(this);
+
   }
 
   /**
@@ -190,6 +207,7 @@ WHS.Camera = class extends WHS.Object {
    * @param {WHS.Camera} source - Source object, that will be applied to this.
    */
   copy(source) {
+
     this.mesh = source.mesh.clone();
 
     this.wrap();
@@ -200,98 +218,122 @@ WHS.Camera = class extends WHS.Object {
     this._type = source._type;
 
     return this;
+
   }
 
   /* Access private data */
 
   setNative(camera) {
+
     return native.set(this, camera);
+
   }
 
   getNative() {
+
     return native.get(this);
+
   }
 
   get position() {
+
     return this.getNative().position;
+
   }
 
   set position(vector3) {
+
     return this.getNative().position.copy(vector3);
+
   }
 
   get rotation() {
+
     return this.getNative().rotation;
+
   }
 
   set rotation(euler) {
+
     return this.getNative().rotation.copy(euler);
+
   }
 
   follow(curve, time = 1000, loop, lookAt) {
-    let _scope = this;
-    let gEnd = time;
+
+    const _scope = this,
+      gEnd = time;
 
     let animation = new WHS.loop(clock => {
-      let u =  clock.getElapsedTime() * 1000 / gEnd;
-      let vec1 = curve.getPoint(u);
-      let vec2 = curve.getPoint((u + 0.01) % 1);
+
+      const u = clock.getElapsedTime() * 1000 / gEnd,
+        vec1 = curve.getPoint(u),
+        vec2 = curve.getPoint((u + 0.01) % 1);
 
       _scope.position.set(vec1.x, vec1.y, vec1.z);
-  
-      if (!lookAt) {
-        _scope.lookAt(vec2);
-      } else if (lookAt instanceof THREE.Vector3) {
-        _scope.lookAt(lookAt);
-      } else if (
-          lookAt instanceof THREE.Curve ||
-          lookAt instanceof THREE.CurvePath
-        ) {
-        _scope.lookAt(lookAt.getPoint(u));
-      }
+
+      if (!lookAt) _scope.lookAt(vec2);
+      else if (lookAt instanceof THREE.Vector3) _scope.lookAt(lookAt);
+      else if (
+          lookAt instanceof THREE.Curve
+          || lookAt instanceof THREE.CurvePath
+        ) _scope.lookAt(lookAt.getPoint(u));
+
     });
 
     animation.start();
 
     if (loop) {
+
       setInterval(() => {
+
         animation.stop();
 
         animation = new WHS.loop(clock => {
-          let u =  clock.getElapsedTime() * 1000 / gEnd;
-          let vec1 = curve.getPoint(u);
-          let vec2 = curve.getPoint((u + 0.01) % 1);
+
+          const u = clock.getElapsedTime() * 1000 / gEnd,
+            vec1 = curve.getPoint(u),
+            vec2 = curve.getPoint((u + 0.01) % 1);
 
           _scope.position.set(vec1.x, vec1.y, vec1.z);
 
-          if (!lookAt) {
-            _scope.lookAt(vec2);
-          } else if (lookAt instanceof THREE.Vector3) {
-            _scope.lookAt(lookAt);
-          } else if (
-              lookAt instanceof THREE.Curve ||
-              lookAt instanceof THREE.CurvePath
-            ) {
-            _scope.lookAt(lookAt.getPoint(u));
-          }
+          if (!lookAt) _scope.lookAt(vec2);
+          else if (lookAt instanceof THREE.Vector3) _scope.lookAt(lookAt);
+          else if (
+              lookAt instanceof THREE.Curve
+              || lookAt instanceof THREE.CurvePath
+            ) _scope.lookAt(lookAt.getPoint(u));
+
         });
 
         animation.start();
+
       }, time);
+
     } else {
+
       setTimeout(() => {
+
         animation.stop();
+
       }, time);
+
     }
+
   }
 
   /* =========== POLYFILL =========== */
 
   lookAt(vector3) {
+
     return this.getNative().lookAt(vector3);
+
   }
 
   getWorldDirection(vector3) {
+
     return this.getNative().getWorldDirection(vector3);
+
   }
+
 };
