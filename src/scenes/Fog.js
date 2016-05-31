17a869cc3df5d2f884ef32e3fc8bf8aafa9ab502
sasha240100 <siteprogcom@gmail.com>
2016-05-31 12:40:55 +0300

    CSF. src/scenes/*.js Updated rules.
    Former-commit-id: 7dbf91ff10a4d784f334931c42c42e3d7e925f59

--- src/scenes/Fog.js
+++ src/scenes/Fog.js
@@ -1,16 +1,16 @@
 import {Fog as _Fog} from 'three';
 
 class Fog {
-	constructor(params = {}) {
-      WHS.API.extend(params, {
-        hex: 0x000000,
-        near: 1,
-        far: 1000
-      });
+  constructor(params = {}) {
+    WHS.API.extend(params, {
+      hex: 0x000000,
+      near: 1,
+      far: 1000
+    });
 
-      this.fog = new _Fog(params.hex, params.near, params.far);
-      this.type = 'fog';
-	}
+    this.fog = new _Fog(params.hex, params.near, params.far);
+    this.type = 'fog';
+  }
 
   addTo(root) {
     root.getScene().fog = this.fog;
--- src/scenes/FogExp2.js
+++ src/scenes/FogExp2.js
@@ -1,13 +1,13 @@
 class FogExp2 {
-	constructor(params = {}) {
+  constructor(params = {}) {
     WHS.API.extend(params, {
       hex: 0x000000,
       density: 0.00025
     });
 
-    this.fog = new THREE.FogExp2( params.hex, params.density);
+    this.fog = new THREE.FogExp2(params.hex, params.density);
     this.type = 'fogexp2';
-	}
+  }
 
   addTo(root) {
     root.getScene().fog = this.fog;
