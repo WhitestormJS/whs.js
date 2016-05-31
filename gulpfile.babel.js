3b3cae445cf09f3adfa249ea6d54e1926f445fb4
sasha240100 <siteprogcom@gmail.com>
2016-05-31 22:08:14 +0300

    CSF. gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -18,44 +18,41 @@ process.env.BABEL_ENV = 'node';
 gulp.task('default', ['build']);
 
 gulp.task('build', (callback) => {
-    runSequence('clean', 'babel', callback);
+  runSequence('clean', 'babel', callback);
 });
 
 gulp.task('babel', () => {
-    return gulp.src(src)
-        .pipe($.cached('babel', {optimizeMemory: true}))
-        .pipe($.if(!isProduction, $.sourcemaps.init()))
-        .pipe($.babel())
-        .on('error', makeBuildErrorHandler('babel'))
-        .pipe($.if(!isProduction, $.sourcemaps.write('.')))
-        .pipe(gulp.dest(dest));
+  return gulp.src(src)
+    .pipe($.cached('babel', {optimizeMemory: true}))
+    .pipe($.if(!isProduction, $.sourcemaps.init()))
+    .pipe($.babel())
+    .on('error', makeBuildErrorHandler('babel'))
+    .pipe($.if(!isProduction, $.sourcemaps.write('.')))
+    .pipe(gulp.dest(dest));
 });
 
 gulp.task('webpack', (callback) => {
-    webpackCompiler.run((error, stats) => {
-        if (error) {
-            throw new $.util.PluginError('webpack', error);
-        }
-
-        $.util.log('[webpack]', stats.toString({colors: true}));
-        callback();
-    });
+  webpackCompiler.run((error, stats) => {
+    if (error) throw new $.util.PluginError('webpack', error);
+    $.util.log('[webpack]', stats.toString({colors: true}));
+    callback();
+  });
 });
 
 gulp.task('lint', () => {
-    gulp.src(src)
-        .pipe($.cached('lint', {optimizeMemory: true}))
-        .pipe($.xo())
-        .on('error', makeBuildErrorHandler('lint'));
+  gulp.src(src)
+    .pipe($.cached('lint', {optimizeMemory: true}))
+    .pipe($.xo())
+    .on('error', makeBuildErrorHandler('lint'));
 });
 
 gulp.task('clean', (callback) => {
-    del(dest).then(() => callback());
+  del(dest).then(() => callback());
 });
 
 function makeBuildErrorHandler(taskName) {
-    return function ({name, message, codeFrame}) {
-        $.util.log(`[${taskName}]`, `${$.util.colors.red(name)} ${message}${codeFrame ? `\n${codeFrame}` : ''}`);
-        this.emit('end');
-    };
+  return function ({name, message, codeFrame}) {
+    $.util.log(`[${taskName}]`, `${$.util.colors.red(name)} ${message}${codeFrame ? `\n${codeFrame}` : ''}`);
+    this.emit('end');
+  };
 }
