be6bb5ebb820473a0a29fe1d539600e4eb468ab4
sasha240100 <siteprogcom@gmail.com>
2016-05-31 12:17:56 +0300

    CSF. src/lights/*.js

--- src/lights/HemisphereLight.js
+++ src/lights/HemisphereLight.js
@@ -1,50 +1,48 @@
 WHS.HemisphereLight = class HemisphereLight extends WHS.Light {
-    /**
-     * Hemisphere light.
-     *
-     * @param {Object} params.light.skyColor - Light sky color.
-     * @param {Object} params.light.groundColor - Light ground color.
-     * @param {Object} params.light.intensity - Light intensity.
-     */
-	constructor( params = {} ) {
 
-		super( params, "hemispherelight" );
+  /**
+   * Hemisphere light.
+   *
+   * @param {Object} params.light.skyColor - Light sky color.
+   * @param {Object} params.light.groundColor - Light ground color.
+   * @param {Object} params.light.intensity - Light intensity.
+   */
+  constructor(params = {}) {
 
-		this.build( params );
+    super(params, 'hemispherelight');
 
-        super.wrap();
-        super.wrapShadow();
+    this.build(params);
 
-	}
+    super.wrap();
+    super.wrapShadow();
 
-    build( params = {} ) {
+  }
 
-        let _scope = this;
+  build(params = {}) {
 
-        return new Promise( (resolve, reject) => {
-            _scope.setNative( new THREE.HemisphereLight(
-                params.light.skyColor,
-                params.light.groundColor,
-                params.light.intensity
-            ) );
+    const _scope = this;
 
-            if ( params.helper )
-                _scope.helper = new THREE.HemisphereLightHelper(
-                    _scope.light,
-                    params.helper.size ? params.helper.size : 0
-                );
+    return new Promise((resolve, reject) => {
 
-            resolve();
-        });
+      _scope.setNative(new THREE.HemisphereLight(
+        params.light.skyColor,
+        params.light.groundColor,
+        params.light.intensity
+      ));
 
-    }
+      if (params.helper) {
 
-}
+        _scope.helper = new THREE.HemisphereLightHelper(
+          _scope.light,
+          params.helper.size ? params.helper.size : 0
+        );
 
-WHS.World.prototype.HemisphereLight = function( params ) {
-    let object = new WHS.HemisphereLight( params );
+      }
 
-    object.addTo( this );
+      resolve();
 
-    return object;
-}
+    });
+
+  }
+
+};
--- src/lights/PointLight.js
+++ src/lights/PointLight.js
@@ -1,52 +1,50 @@
 WHS.PointLight = class PointLight extends WHS.Light {
-    /**
-     * Point light.
-     *
-     * @param {Object} params.light.color - Light color.
-     * @param {Object} params.light.intensity - Light intensity.
-     * @param {Object} params.light.distance - Light distance.
-     * @param {Object} params.light.decay - Light decay.
-     */
-	constructor( params = {} ) {
 
-		super( params, "pointlight" );
+  /**
+   * Point light.
+   *
+   * @param {Object} params.light.color - Light color.
+   * @param {Object} params.light.intensity - Light intensity.
+   * @param {Object} params.light.distance - Light distance.
+   * @param {Object} params.light.decay - Light decay.
+   */
+  constructor(params = {}) {
 
-		this.build( params );
+    super(params, 'pointlight');
 
-        super.wrap();
-        super.wrapShadow();
+    this.build(params);
 
-	}
+    super.wrap();
+    super.wrapShadow();
 
-    build( params = {} ) {
+  }
 
-        let _scope = this;
+  build(params = {}) {
 
-        return new Promise( (resolve, reject) => {
-            _scope.setNative( new THREE.PointLight(
-                params.light.color,
-                params.light.intensity,
-                params.light.distance,
-                params.light.decay
-            ) );
+    const _scope = this;
 
-            if ( params.helper )
-                _scope.helper = new THREE.PointLightHelper(
-                    _scope.light,
-                    params.helper.size ? params.helper.size : 0
-                );
+    return new Promise((resolve, reject) => {
 
-            resolve();
-        });
+      _scope.setNative(new THREE.PointLight(
+        params.light.color,
+        params.light.intensity,
+        params.light.distance,
+        params.light.decay
+      ));
 
-    }
+      if (params.helper) {
 
-}
+        _scope.helper = new THREE.PointLightHelper(
+          _scope.light,
+          params.helper.size ? params.helper.size : 0
+        );
 
-WHS.World.prototype.PointLight = function( params ) {
-    let object = new WHS.PointLight( params );
+      }
 
-    object.addTo( this );
+      resolve();
 
-    return object;
-}
+    });
+
+  }
+
+};
