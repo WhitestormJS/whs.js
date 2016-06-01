248d71c615a65fa867d8382157eec5683b37152c
sasha240100 <siteprogcom@gmail.com>
2016-06-01 13:55:41 +0300

    Fix webpack.
    Former-commit-id: e189b5f4694a3d7d1e866ffce21346c33f4e24b7

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -4,37 +4,38 @@ import webpack from 'webpack';
 process.env.BABEL_ENV = 'browser';
 
 function config({production}) {
-    return {
-        devtool: production ? 'hidden-source-map' : 'eval-source-map',
-        entry: './src/index.js',
-        output: {
-            path: join(__dirname, 'dist'),
-            library: 'WHS',
-            libraryTarget: 'umd'
-        },
-        module: {
-            loaders: [
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    loader: 'babel'
-                }
-            ]
-        },
-        plugins: production ?
-            [
-                new webpack.optimize.DedupePlugin(),
-                new webpack.optimize.UglifyJsPlugin({
-                    mangle: false,
-                    compress: {
-                        warnings: false
-                    }
-                })
-            ] :
-            []
-    };
+  return {
+    devtool: production ? 'hidden-source-map' : 'eval-source-map',
+    entry: './src/index.js',
+    output: {
+      path: join(__dirname, 'build'),
+      filename: 'whitestorm.js',
+      library: 'WHS',
+      libraryTarget: 'umd'
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel'
+        }
+      ]
+    },
+    plugins: production
+      ? [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+          mangle: false,
+          compress: {
+            warnings: false
+          }
+        })
+      ]
+      : []
+  };
 }
 
 export {
-    config as default
+  config as default
 };
