b14718b94e96a94bd529619058b8aa23d29fb196
sasha240100 <siteprogcom@gmail.com>
2016-06-01 16:11:55 +0300

    CSF. api.js
    Former-commit-id: fe2310e47f02f567aae66f5958664425c19897b6

--- src/extras/api.js
+++ src/extras/api.js
@@ -1,6 +1,26 @@
-import THREE from "three";
-import Physijs from "whitestormjs-physijs";
-import {loadJson, loadTexture, loadFont} from "../utils/loader";
+import {
+  RepeatWrapping as TRepeatWrapping,
+  NearestFilter as TNearestFilter,
+  LinearMipMapLinearFilter as TLinearMipMapLinearFilter,
+  // Materials.
+  MeshBasicMaterial as TMeshBasicMaterial,
+  LineBasicMaterial as TLineBasicMaterial,
+  LineDashedMaterial as TLineDashedMaterial,
+  Material as TMaterial,
+  MeshDepthMaterial as TMeshDepthMaterial,
+  MeshFaceMaterial as TMeshFaceMaterial,
+  MeshLambertMaterial as TMeshLambertMaterial,
+  MeshNormalMaterial as TMeshNormalMaterial,
+  MeshPhongMaterial as TMeshPhongMaterial,
+  PointCloudMaterial as TPointCloudMaterial,
+  RawShaderMaterial as TRawShaderMaterial,
+  ShaderMaterial as TShaderMaterial,
+  SpriteCanvasMaterial as TSpriteCanvasMaterial,
+  SpriteMaterial as TSpriteMaterial
+} from 'three';
+
+import {createMaterial as pcreateMaterial} from 'whitestormjs-physijs';
+import {loadJson, loadTexture, loadFont} from '../utils/loader';
 
 const extend = Object.assign;
 
@@ -8,37 +28,32 @@ const texture = (url, options) => {
   const texture = loadTexture(url);
 
   if (options) {
-
     const opt = Object.assign({}, options, {
-
       offset: {
         x: 0,
         y: 0
       },
-
       repeat: {
         x: 1,
         y: 1
       }
-
     });
 
-    texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+    texture.wrapS = texture.wrapT = TRepeatWrapping;
 
     texture.offset.set(opt.offset.x, opt.offset.y);
     texture.repeat.set(opt.repeat.x, opt.repeat.y);
 
-    texture.magFilter = THREE.NearestFilter;
-    texture.minFilter = THREE.LinearMipMapLinearFilter;
-
+    texture.magFilter = TNearestFilter;
+    texture.minFilter = TLinearMipMapLinearFilter;
   }
 
   return texture;
 };
 
 const loadMaterial = (material = {}, isPhysics = true) => {
-  if (typeof material.kind !== "string")
-    console.error("Type of material is undefined or not a string. @loadMaterial");
+  if (typeof material.kind !== 'string')
+    console.error('Type of material is undefined or not a string. @loadMaterial');
 
   const scope = {
     _type: material.kind,
@@ -52,86 +67,88 @@ const loadMaterial = (material = {}, isPhysics = true) => {
 
   if (material.texture) material.map = texture(material.texture);
 
-  let params = Object.assign({}, material);
+  const params = Object.assign({}, material);
 
-  delete params["kind"];
+  delete params.kind;
 
-  delete params["friction"];
-  delete params["fri"];
+  delete params.friction;
+  delete params.fri;
 
-  delete params["restitution"];
-  delete params["rest"];
+  delete params.restitution;
+  delete params.rest;
 
-  delete params["useCustomMaterial"];
-  delete params["useVertexColors"];
+  delete params.useCustomMaterial;
+  delete params.useVertexColors;
 
   switch (material.kind) {
-    case "basic":
-      scope._material = new THREE.MeshBasicMaterial(params);
+    case 'basic':
+      scope._material = new TMeshBasicMaterial(params);
       break;
 
-    case "linebasic":
-      scope._material = new THREE.LineBasicMaterial(params);
+    case 'linebasic':
+      scope._material = new TLineBasicMaterial(params);
       break;
 
-    case "linedashed":
-      scope._material = new THREE.LineDashedMaterial(params);
+    case 'linedashed':
+      scope._material = new TLineDashedMaterial(params);
       break;
 
-    case "material":
-      scope._material = new THREE.Material(params);
+    case 'material':
+      scope._material = new TMaterial(params);
       break;
 
-    case "depth":
-      scope._material = new THREE.MeshDepthMaterial(params);
+    case 'depth':
+      scope._material = new TMeshDepthMaterial(params);
       break;
 
-    case "face":
-      scope._material = new THREE.MeshFaceMaterial(params);
+    case 'face':
+      scope._material = new TMeshFaceMaterial(params);
       break;
 
-    case "lambert":
-      scope._material = new THREE.MeshLambertMaterial(params);
+    case 'lambert':
+      scope._material = new TMeshLambertMaterial(params);
       break;
 
-    case "normal":
-      scope._material = new THREE.MeshNormalMaterial(params);
+    case 'normal':
+      scope._material = new TMeshNormalMaterial(params);
       break;
 
-    case "phong":
-      scope._material = new THREE.MeshPhongMaterial(params);
+    case 'phong':
+      scope._material = new TMeshPhongMaterial(params);
       break;
 
-    case "pointcloud":
-      scope._material = new THREE.PointCloudMaterial(params);
+    case 'pointcloud':
+      scope._material = new TPointCloudMaterial(params);
       break;
 
-    case "rawshader":
-      scope._material = new THREE.RawShaderMaterial(params);
+    case 'rawshader':
+      scope._material = new TRawShaderMaterial(params);
       break;
 
-    case "shader":
-      scope._material = new THREE.ShaderMaterial(params);
+    case 'shader':
+      scope._material = new TShaderMaterial(params);
       break;
 
-    case "spritecanvas":
-      scope._material = new THREE.SpriteCanvasMaterial(params);
+    case 'spritecanvas':
+      scope._material = new TSpriteCanvasMaterial(params);
       break;
 
-    case "sprite":
-      scope._material = new THREE.SpriteMaterial(params);
+    case 'sprite':
+      scope._material = new TSpriteMaterial(params);
       break;
+
+    default:
   }
 
-  if (isPhysics)
-    scope._materialP = Physijs.createMaterial(
+  if (isPhysics) {
+    scope._materialP = pcreateMaterial(
       scope._material,
       scope._friction,
       scope._restitution
     );
+  }
 
   return scope;
-
 };
 
 export {
@@ -139,5 +156,6 @@ export {
   loadJson,
   loadTexture,
   texture,
+  extend,
   loadMaterial
-}
+};
