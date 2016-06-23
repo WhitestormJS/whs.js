29ff27c9d50c88a4955e1f02bcf6b9bb3e7ea60d
Estuardo Diaz <esjoando555@gmail.com>
2016-06-23 17:15:16 -0600

    Fixes missconfiguration for gulp task
    Former-commit-id: 58fd326ba60afc61a3568acd7c48ff1705977588

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,10 +1,15 @@
 module.exports = function(config){
   config.set({
-    basePath: '../..',
+    basePath: '../',
     frameworks: ['benchmark'],
     reporters: ['benchmark'],
     files: [
-      '/**/*.spec.js'
-    ]
+      'test/**/*.spec.js'
+    ],
+    brosers: [
+      'Chrome'
+    ],
+    autoWatch: false,
+    singleRun: true
   })
 }
