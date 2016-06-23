8db0bdc7bcecbe7f68a024fddc5172ebc71147c1
Estuardo Diaz <esjoando555@gmail.com>
2016-06-23 17:00:21 -0600

    Sets up test task
    Former-commit-id: 471b632de7762f7e468444a118e8c17a577e3e71

---
+++ test/unit/hello.spec.js
@@ -0,0 +1,18 @@
+var suite = new Benchmark.Suite;
+
+// add tests
+suite.add('RegExp#test', function() {
+  /o/.test('Hello World!');
+})
+.add('String#indexOf', function() {
+  'Hello World!'.indexOf('o') > -1;
+})
+// add listeners
+.on('cycle', function(event) {
+  console.log(String(event.target));
+})
+.on('complete', function() {
+  console.log('Fastest is ' + this.filter('fastest').map('name'));
+})
+// run async
+.run({ 'async': true });
