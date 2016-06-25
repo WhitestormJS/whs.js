5b969ef30934e13099ef9845de9e92efb4f705f9
sasha240100 <siteprogcom@gmail.com>
2016-06-25 13:23:47 +0300

    WIP.
    Former-commit-id: d6895b0d0ae510f56325a7ec1b6ef42fa4bc6e2a

---
+++ test/test-main.js
@@ -0,0 +1,26 @@
+const tests = [];
+
+for (let file in window.__karma__.files)
+  if (/Spec\.js$/.test(file)) tests.push(file);
+
+requirejs.config({
+  // Karma serves files from '/base'
+  baseUrl: '/base/src',
+
+  paths: {
+    whitestormjs: '../lib/index'
+  },
+
+  shim: {
+    underscore: {
+      exports: '_'
+    }
+  },
+
+  // ask Require.js to load these files (all our tests)
+  deps: tests,
+
+  // start test run, once Require.js is done
+  callback: window.__karma__.start
+});
+
