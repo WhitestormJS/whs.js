8fbd09e79abe8089d9896823b35465cb46175682
sasha240100 <siteprogcom@gmail.com>
2016-06-27 12:00:54 +0300

    HingeConstraint
    Former-commit-id: 032c7551eeaddb8496ec742193bc1385671b4968

---
+++ src/physics/constraints/HingeConstraint.js
@@ -0,0 +1,37 @@
+import {convertWorldPositionToObject, getObjectId} from '../api';
+
+export default class HingeConstraint {
+  constructor(objecta, objectb, position, axis) {
+    if (axis === undefined) {
+      axis = position;
+      position = objectb;
+      objectb = undefined;
+    }
+
+    this.type = 'hinge';
+    this.appliedImpulse = 0;
+    this.id = getObjectId();
+    this.scene = objecta.parent;
+    this.objecta = objecta._physijs.id;
+    this.positiona = convertWorldPositionToObject(position, objecta).clone();
+    this.position = position.clone();
+    this.axis = axis;
+
+    if (objectb) {
+      this.objectb = objectb._physijs.id;
+      this.positionb = convertWorldPositionToObject(position, objectb).clone();
+    }
+  }
+
+  getDefinition() {
+    return {
+      type: this.type,
+      id: this.id,
+      objecta: this.objecta,
+      objectb: this.objectb,
+      positiona: this.positiona,
+      positionb: this.positionb,
+      axis: this.axis
+    };
+  }
+}
