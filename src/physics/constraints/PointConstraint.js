fba68ab2b9f90f5f5cbf1e64326e5eef5ede79dd
sasha240100 <siteprogcom@gmail.com>
2016-06-27 11:57:17 +0300

    API & PointConstraint

---
+++ src/physics/constraints/PointConstraint.js
@@ -0,0 +1,32 @@
+import {convertWorldPositionToObject} from '../api';
+
+export default class PointConstraint {
+  constructor(objecta, objectb, position) {
+    if (position === undefined) {
+      position = objectb;
+      objectb = undefined;
+    }
+
+    this.type = 'point';
+    this.appliedImpulse = 0;
+    this.id = getObjectId();
+    this.objecta = objecta._physijs.id;
+    this.positiona = convertWorldPositionToObject(position, objecta).clone();
+
+    if (objectb) {
+      this.objectb = objectb._physijs.id;
+      this.positionb = convertWorldPositionToObject(position, objectb).clone();
+    }
+  }
+
+  getDefinition() {
+    return {
+      type: this.type,
+      id: this.id,
+      objecta: this.objecta,
+      objectb: this.objectb,
+      positiona: this.positiona,
+      positionb: this.positionb
+    };
+  }
+}
