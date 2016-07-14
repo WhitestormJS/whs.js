ce3956f4ab10e66ef482453264f442615fb0f12a
sasha240100 <alexbuzin88@gmail.com>
2016-07-14 23:08:24 +0300

    WHS.Points + example
    Former-commit-id: a8a55624c50d06a82b7f73c42ecb22c837d2f12e

---
+++ examples/design/points/script.js
@@ -0,0 +1,68 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
+window.GAME = new WHS.World({
+  autoresize: true,
+
+  gravity: {
+    x: 0,
+    y: -10,
+    z: 0
+  },
+
+  camera: {
+    far: 2000,
+    near: 1,
+    z: 20,
+    x: -8,
+    y: 5,
+
+    aspect: 45
+  },
+
+  background: {
+    color: 0xffffff
+  }
+});
+
+var data = new Float32Array(3993000);
+var colors = new Float32Array(3993000);
+
+var i = 0;
+for (var x = 0; x <= 100; x++) {
+  for (var y = 0; y <= 100; y++) {
+    for (var z = 0; z <= 100; z++) {
+      data[i * 3] = x;
+      data[i * 3 + 1] = y;
+      data[i * 3 + 2] = z;
+      colors[i * 3] = x / 100;
+      colors[i * 3 + 1] = y / 100;
+      colors[i * 3 + 2] = z / 100;
+      i++;
+    }
+  }
+}
+
+var geom = new THREE.BufferGeometry();
+
+geom.addAttribute('position', new THREE.BufferAttribute(data, 3));
+geom.addAttribute('color', new THREE.BufferAttribute(colors, 3));
+
+window.points = new WHS.Points({
+  geometry: geom,
+
+  material: {
+    kind: 'points',
+    vertexColors: THREE.VertexColors,
+    size: 0.1
+  }
+});
+
+points.addTo(GAME);
+
+GAME.setControls(WHS.orbitControls(new THREE.Vector3(50, 50, 50)));
+
+// Start rendering.
+GAME.start();
+
+},{}]},{},[1]);
