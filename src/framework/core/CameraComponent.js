eb74a78f87396677ea3e5d173431b3b85aa81c84
sasha240100 <alexbuzin88@gmail.com>
2016-10-15 22:33:03 +0300

    CameraComponent structure update.
    Former-commit-id: c70aa4bebbff6b4cae229b0164531950fd515d19

--- src/framework/core/CameraComponent.js
+++ src/framework/core/CameraComponent.js
@@ -1,81 +1,77 @@
 import * as THREE from 'three';
 import {$wrap, $defaults, $extend, $define} from '../utils/ComponentUtils';
 
-function CameraComponent(target) {
-  $defaults(target, {
-    camera: {
-      fov: 45,
-      aspect: window.innerWidth / window.innerHeight,
-      near: 1,
-      far: 1000,
-      left: window.innerWidth / -2,
-      right: window.innerWidth / 2,
-      top: window.innerHeight / 2,
-      bottom: window.innerHeight / -2,
-      cubeResolution: 128
-    },
+import {extend} from '../utils/index';
 
-    helper: false,
-
-    position: {x: 0, y: 0, z: 0},
-    rotation: {x: 0, y: 0, z: 0},
-    target: {x: 0, y: 0, z: 0}
-  });
-
-  $define(target, {
-    position: {
-      get: function() {
-        return this.native.position;
+function CameraComponent(targetComponent) {
+  const resultComponent = class CameraComponentEnhance extends targetComponent {
+    static defautls = extend(targetComponent.defaults, {
+      camera: {
+        fov: 45,
+        aspect: window.innerWidth / window.innerHeight,
+        near: 1,
+        far: 1000,
+        left: window.innerWidth / -2,
+        right: window.innerWidth / 2,
+        top: window.innerHeight / 2,
+        bottom: window.innerHeight / -2,
+        cubeResolution: 128
       },
 
-      set: function(vector3) {
-        this.native.position.copy(vector3);
-        return this.native.position;
-      }
-    },
+      helper: false,
 
-    quaternion: {
-      get: function() {
-        return this.native.quaternion;
-      },
+      position: {x: 0, y: 0, z: 0},
+      rotation: {x: 0, y: 0, z: 0},
+      target: {x: 0, y: 0, z: 0}
+    });
 
-      set: function(quaternion) {
-        this.native.quaternion.copy(quaternion);
-        return this.native.quaternion;
-      }
-    },
+    constructor(...props) {
+      super(...props);
 
-    rotation: {
-      get: function() {
-        return this._native.rotation;
-      },
-
-      set: function(euler) {
-        this.native.rotation.copy(euler);
-        return this.native.rotation;
-      }
-    },
-
-    target: {
-      get: function() {
-        return this.native.target;
-      },
-
-      set: function(vector3) {
-        if (vector3 instanceof THREE.Object3D)
-          this.native.target.copy(vector3); // THREE.Object3D in this case.
-        else this.native.target.position.copy(vector3);
-      }
+      this.helper = null;
+      if (this.native instanceof THREE.Object3D) this.params = CameraComponentEnhance.defaults;
     }
-  })
 
+    get position() {
+      return this.native.position;
+    }
 
-  $extend(target, {
-    /* Three.js */
+    set position(vector3) {
+      this.native.position.copy(vector3);
+      return this.native.position;
+    }
+
+    get quaternion() {
+      return this.native.quaternion;
+    }
+
+    set quaternion(quaternion) {
+      this.native.quaternion.copy(quaternion);
+      return this.native.quaternion;
+    }
+
+    get rotation() {
+      return this._native.rotation;
+    }
+
+    set rotation(euler) {
+      this.native.rotation.copy(euler);
+      return this.native.rotation;
+    }
+
+    get target() {
+      return this.native.target;
+    }
+
+    set target(vector3) {
+      if (vector3 instanceof THREE.Object3D)
+        this.native.target.copy(vector3); // THREE.Object3D in this case.
+      else this.native.target.position.copy(vector3);
+    }
 
     lookAt(...args) {
       return this.native.lookAt(...args);
-    },
+    }
 
     copy(source) {
       if (source.native) {
@@ -94,14 +90,9 @@ function CameraComponent(target) {
 
       return this;
     }
-  });
+  }
 
-  $wrap(target).onCallConstructor(scope => {
-    scope.helper = null;
-    if (scope.native instanceof THREE.Object3D) scope.params = scope.defaults;
-  });
-
-  $wrap(target).onCallWrap((scope, ...tags) => {
+  $wrap(resultComponent).onCallWrap((scope, ...tags) => {
     const _native = scope.native;
     const _params = scope.params;
 
@@ -120,8 +111,8 @@ function CameraComponent(target) {
     if (_params.useTarget) scope.lookAt(_params.target);
     if (_params.helper) scope.helper = new THREE.CameraHelper(_native);
   });
-  // console.log('Target :', target);
-  return target;
+
+  return resultComponent;
 }
 
 export {
