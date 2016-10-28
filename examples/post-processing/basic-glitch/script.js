1d2811d69ef078e4949c7a2c00dc0afb210c5713
Yannis Torres <yann.torres@gmail.com>
2016-10-29 01:23:59 +0200

    WIP. updated example builds.
    Former-commit-id: 0d508669d83e7f6a0b1ad2c96b26efcfa384ecf5

--- examples/post-processing/basic-glitch/script.js
+++ examples/post-processing/basic-glitch/script.js
@@ -1,7 +1,13 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-"use strict";
+'use strict';
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _globals = require('./globals');
+
+var UTILS = _interopRequireWildcard(_globals);
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -14,6 +20,8 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 // -----------------------------------------------------------------------------
 var conf = {
   world: {
+    autoresize: true,
+
     gravity: {
       x: 0,
       y: -100,
@@ -24,7 +32,26 @@ var conf = {
       far: 10000,
       y: 10,
       z: 30
+    },
+
+    init: {
+      rendering: false
+    },
+
+    rendering: {
+      background: {
+        color: 0x162129
+      },
+
+      renderer: {
+        antialias: true
+      }
+    },
+
+    shadowmap: {
+      type: THREE.PCFSoftShadowMap
     }
+
   },
 
   sphere: {
@@ -37,7 +64,7 @@ var conf = {
     mass: 10,
 
     material: {
-      color: 0xffffff,
+      color: 0xF2F2F2,
       kind: 'basic'
     },
 
@@ -57,8 +84,8 @@ var conf = {
     mass: 0,
 
     material: {
-      color: 0xff0000,
-      kind: "basic"
+      color: 0x447F8B,
+      kind: 'basic'
     },
 
     position: {
@@ -69,28 +96,6 @@ var conf = {
 
     rotation: {
       x: -Math.PI / 2
-    }
-  },
-
-  rendering: {
-    renderTarget: {
-      minFilter: THREE.LinearFilter,
-      magFilter: THREE.LinearFilter,
-      format: THREE.RGBAFormat,
-      stencilBuffer: false
-    },
-
-    background: {
-      color: 0xffffff
-    }
-  },
-
-  postProcessor: {
-    renderTarget: {
-      minFilter: THREE.LinearFilter,
-      magFilter: THREE.LinearFilter,
-      format: THREE.RGBAFormat,
-      stencilBuffer: false
     }
   }
 };
@@ -113,9 +118,9 @@ var DigitalGlitchShader = {
     col_s: { value: 0.05 }
   },
 
-  vertexShader: "\n    varying vec2 vUv;\n\n    void main() {\n      vUv = uv;\n      gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );\n    }\n  ",
+  vertexShader: '\n    varying vec2 vUv;\n\n    void main() {\n      vUv = uv;\n      gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );\n    }\n  ',
 
-  fragmentShader: "\n    uniform int byp; //should we apply the glitch ?\n\n    uniform sampler2D tDiffuse;\n    uniform sampler2D tDisp;\n\n    uniform float amount;\n    uniform float angle;\n    uniform float seed;\n    uniform float seed_x;\n    uniform float seed_y;\n    uniform float distortion_x;\n    uniform float distortion_y;\n    uniform float col_s;\n\n    varying vec2 vUv;\n\n\n    float rand(vec2 co){\n      return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);\n    }\n\n    void main() {\n      if(byp<1) {\n        vec2 p = vUv;\n        float xs = floor(gl_FragCoord.x / 0.5);\n        float ys = floor(gl_FragCoord.y / 0.5);\n        //based on staffantans glitch shader for unity https://github.com/staffantan/unityglitch\n        vec4 normal = texture2D (tDisp, p*seed*seed);\n\n        if(p.y<distortion_x+col_s && p.y>distortion_x-col_s*seed) {\n          if(seed_x>0.){\n            p.y = 1. - (p.y + distortion_y);\n          }\n          else {\n            p.y = distortion_y;\n          }\n        }\n\n        if(p.x<distortion_y+col_s && p.x>distortion_y-col_s*seed) {\n          if(seed_y>0.){\n            p.x=distortion_x;\n          }\n          else {\n            p.x = 1. - (p.x + distortion_x);\n          }\n        }\n\n        p.x+=normal.x*seed_x*(seed/5.);\n        p.y+=normal.y*seed_y*(seed/5.);\n\n        //base from RGB shift shader\n\n        vec2 offset = amount * vec2( cos(angle), sin(angle));\n        vec4 cr = texture2D(tDiffuse, p + offset);\n        vec4 cga = texture2D(tDiffuse, p);\n        vec4 cb = texture2D(tDiffuse, p - offset);\n        gl_FragColor = vec4(cr.r, cga.g, cb.b, cga.a);\n\n        //add noise\n        vec4 snow = 200.*amount*vec4(rand(vec2(xs * seed,ys * seed*50.))*0.2);\n        gl_FragColor = gl_FragColor+ snow;\n      }\n      else {\n        gl_FragColor = texture2D (tDiffuse, vUv);\n      }\n    }\n  "
+  fragmentShader: '\n    uniform int byp; //should we apply the glitch ?\n\n    uniform sampler2D tDiffuse;\n    uniform sampler2D tDisp;\n\n    uniform float amount;\n    uniform float angle;\n    uniform float seed;\n    uniform float seed_x;\n    uniform float seed_y;\n    uniform float distortion_x;\n    uniform float distortion_y;\n    uniform float col_s;\n\n    varying vec2 vUv;\n\n\n    float rand(vec2 co){\n      return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);\n    }\n\n    void main() {\n      if(byp<1) {\n        vec2 p = vUv;\n        float xs = floor(gl_FragCoord.x / 0.5);\n        float ys = floor(gl_FragCoord.y / 0.5);\n        //based on staffantans glitch shader for unity https://github.com/staffantan/unityglitch\n        vec4 normal = texture2D (tDisp, p*seed*seed);\n\n        if(p.y<distortion_x+col_s && p.y>distortion_x-col_s*seed) {\n          if(seed_x>0.){\n            p.y = 1. - (p.y + distortion_y);\n          }\n          else {\n            p.y = distortion_y;\n          }\n        }\n\n        if(p.x<distortion_y+col_s && p.x>distortion_y-col_s*seed) {\n          if(seed_y>0.){\n            p.x=distortion_x;\n          }\n          else {\n            p.x = 1. - (p.x + distortion_x);\n          }\n        }\n\n        p.x+=normal.x*seed_x*(seed/5.);\n        p.y+=normal.y*seed_y*(seed/5.);\n\n        //base from RGB shift shader\n\n        vec2 offset = amount * vec2( cos(angle), sin(angle));\n        vec4 cr = texture2D(tDiffuse, p + offset);\n        vec4 cga = texture2D(tDiffuse, p);\n        vec4 cb = texture2D(tDiffuse, p - offset);\n        gl_FragColor = vec4(cr.r, cga.g, cb.b, cga.a);\n\n        //add noise\n        vec4 snow = 200.*amount*vec4(rand(vec2(xs * seed,ys * seed*50.))*0.2);\n        gl_FragColor = gl_FragColor+ snow;\n      }\n      else {\n        gl_FragColor = texture2D (tDiffuse, vUv);\n      }\n    }\n  '
 };
 
 var GlitchPass = function (_WHS$Pass) {
@@ -154,7 +159,7 @@ var GlitchPass = function (_WHS$Pass) {
   }
 
   _createClass(GlitchPass, [{
-    key: "render",
+    key: 'render',
     value: function render(renderer, writeBuffer, readBuffer, delta, maskActive) {
       this.uniforms["tDiffuse"].value = readBuffer.texture;
       this.uniforms['seed'].value = Math.random(); //default seeding
@@ -190,12 +195,12 @@ var GlitchPass = function (_WHS$Pass) {
       }
     }
   }, {
-    key: "generateTrigger",
+    key: 'generateTrigger',
     value: function generateTrigger() {
       this.randX = THREE.Math.randInt(120, 240);
     }
   }, {
-    key: "generateHeightmap",
+    key: 'generateHeightmap',
     value: function generateHeightmap(dt_size) {
       var data_arr = new Float32Array(dt_size * dt_size * 3);
       var length = dt_size * dt_size;
@@ -234,10 +239,11 @@ var Game = function () {
   }
 
   _createClass(Game, [{
-    key: "createPostProcessing",
+    key: 'createPostProcessing',
     value: function createPostProcessing() {
       var computedWidth = Number(this.world.params.width * this.world.params.rWidth).toFixed();
       var computedHeight = Number(this.world.params.height * this.world.params.rHeight).toFixed();
+
       var renderingPluginParams = {
         width: computedWidth,
         height: computedHeight,
@@ -248,29 +254,22 @@ var Game = function () {
         },
 
         background: {
-          color: 0xffffff
-        },
-
-        renderer: {
-          minFilter: THREE.LinearFilter,
-          magFilter: THREE.LinearFilter,
-          format: THREE.RGBAFormat,
-          stencilBuffer: false
+          color: 0x162129
         }
       };
 
-      this.postProcessor = new WHS.PostProcessor(renderingPluginParams, this.world);
-      this.world.renderingPlugin = this.postProcessor;
+      this.world.renderingPlugin = new WHS.PostProcessor(renderingPluginParams);
+      this.postProcessor = this.world.renderingPlugin;
 
-      // this.postProcessor.createRenderPass();
-      // this.postProcessor.createPass(composer => {
-      //   const pass = new GlitchPass('Glitch');
-      //   pass.renderToScreen = true;
-      //   composer.addPass(pass);
-      // });
+      this.postProcessor.createRenderPass(false);
+      this.postProcessor.createPass(function (composer) {
+        var pass = new GlitchPass('Glitch');
+        pass.renderToScreen = true;
+        composer.addPass(pass);
+      });
     }
   }, {
-    key: "createGeometry",
+    key: 'createGeometry',
     value: function createGeometry() {
       this.plane = new WHS.Plane(this.options.plane);
       this.plane.addTo(this.world);
@@ -279,9 +278,10 @@ var Game = function () {
       this.sphere.addTo(this.world);
     }
   }, {
-    key: "start",
+    key: 'start',
     value: function start() {
       this.world.start();
+      this.world.setControls(new WHS.OrbitControls());
     }
   }]);
 
@@ -313,5 +313,119 @@ function configure() {
 configure().then(function () {
   return bootstrap();
 });
+
+},{"./globals":2}],2:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.addAmbient = addAmbient;
+exports.addBasicLights = addBasicLights;
+exports.addPlane = addPlane;
+exports.addBoxPlane = addBoxPlane;
+var $world = exports.$world = {
+  stats: "fps", // fps, ms, mb or false if not need.
+  autoresize: "window",
+
+  gravity: { // Physic gravity.
+    x: 0,
+    y: -100,
+    z: 0
+  },
+
+  camera: {
+    z: 50, // Move camera.
+    y: 10
+  },
+
+  rendering: {
+    background: {
+      color: 0x162129
+    },
+
+    renderer: {
+      antialias: true
+    }
+  },
+
+  shadowmap: {
+    type: THREE.PCFSoftShadowMap
+  }
+};
+
+var $colors = exports.$colors = {
+  bg: 0x162129,
+  plane: 0x447F8B,
+  mesh: 0xF2F2F2
+};
+
+function addAmbient(world, intensity) {
+  new WHS.AmbientLight({
+    light: {
+      intensity: intensity
+    }
+  }).addTo(world);
+}
+
+function addBasicLights(world) {
+  var intensity = arguments.length <= 1 || arguments[1] === undefined ? 0.5 : arguments[1];
+  var position = arguments.length <= 2 || arguments[2] === undefined ? [0, 10, 10] : arguments[2];
+
+  new WHS.PointLight({
+    light: {
+      intensity: intensity
+    },
+
+    shadowmap: {
+      fov: 90
+    },
+
+    position: [0, 10, 10]
+  }).addTo(world);
+
+  addAmbient(world, 1 - intensity);
+}
+
+function addPlane(world) {
+  var size = arguments.length <= 1 || arguments[1] === undefined ? 100 : arguments[1];
+
+  new WHS.Plane({
+    geometry: {
+      width: size,
+      height: size
+    },
+
+    mass: 0,
+
+    material: {
+      color: 0x447F8B,
+      kind: 'phong'
+    },
+
+    rotation: {
+      x: -Math.PI / 2
+    }
+  }).addTo(world);
+}
+
+function addBoxPlane(world) {
+  var size = arguments.length <= 1 || arguments[1] === undefined ? 100 : arguments[1];
+
+  new WHS.Box({
+    geometry: {
+      width: size,
+      height: 1,
+      depth: size
+    },
+
+    mass: 0,
+
+    material: {
+      color: 0x447F8B,
+      kind: 'phong'
+    }
+  }).addTo(world);
+}
 
 },{}]},{},[1]);
