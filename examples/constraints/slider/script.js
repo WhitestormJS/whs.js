78131d0ee0d798e97de1fb58940155dc221d4e1d
sasha240100 <alexbuzin88@gmail.com>
2016-11-08 13:38:38 +0200

    Fix examples
    Former-commit-id: 3a5dc7ac65a5398105f928ea0cf0c5f1a7752f03

--- examples/constraints/slider/script.js
+++ examples/constraints/slider/script.js
@@ -134,7 +134,7 @@ function addBasicLights(world) {
 
   addAmbient(world, 1 - intensity);
 
-  return new WHS.SpotLight({
+  return new WHS.PointLight({
     light: {
       intensity: intensity,
       distance: distance
@@ -142,10 +142,6 @@ function addBasicLights(world) {
 
     shadowmap: {
       fov: 90
-    },
-
-    target: {
-      x: 50
     },
 
     position: position
--- examples/physics/filtering/script.js
+++ examples/physics/filtering/script.js
@@ -138,7 +138,7 @@ function addBasicLights(world) {
 
   addAmbient(world, 1 - intensity);
 
-  return new WHS.SpotLight({
+  return new WHS.PointLight({
     light: {
       intensity: intensity,
       distance: distance
@@ -146,10 +146,6 @@ function addBasicLights(world) {
 
     shadowmap: {
       fov: 90
-    },
-
-    target: {
-      x: 50
     },
 
     position: position
