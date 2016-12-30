7c933380d81d220786647b0b1af1450201283b55
Alexander Buzin <alexbuzin88@gmail.com>
2016-12-30 23:08:22 +0200

    WIP

---
+++ src/framework/modules/world/index.js
@@ -0,0 +1,13 @@
+import {ElementModule} from './ElementModule';
+import {CameraModule} from './CameraModule';
+import {HelpersModule} from './HelpersModule';
+import {RenderingModule} from './RenderingModule';
+import {SceneModule} from './SceneModule';
+
+export const modules = {
+  ElementModule,
+  CameraModule,
+  HelpersModule,
+  RenderingModule,
+  SceneModule
+};
