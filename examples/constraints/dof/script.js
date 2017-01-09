cbdd67e42fa48682273642b5d1f72c505bbfaf79
sasha240100 <alexbuzin88@gmail.com>
2017-01-09 03:54:10 +0200

    Replace swig rendering engine with pug
    Former-commit-id: 1180e65c0209b26897944c7f4e920f8dbfe7db2a

--- examples/constraints/dof/script.js
+++ examples/constraints/dof/script.js
@@ -1,35 +1,27 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-'use strict';
+import * as UTILS from '../../globals';
 
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
-
-var _globals = require('./globals');
-
-var UTILS = _interopRequireWildcard(_globals);
-
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
-
-var world = new (PHYSICS.$world(WHS.World))(_extends({}, UTILS.$world, {
+const world = new (PHYSICS.$world(WHS.World))({
+  ...UTILS.$world,
 
   physics: {
-    ammo: 'http://localhost:8001/vendor/ammo.js'
+    ammo: process.ammoPath
   },
 
   camera: {
     far: 10000,
     position: [0, 40, 70]
   }
-}));
+});
 
-var halfMat = {
+const halfMat = {
   kind: 'phong',
   transparent: true,
   opacity: 0.5
 };
 
-var PhysicsBox = PHYSICS.$rigidBody(WHS.Box, PHYSICS.BOX);
+const PhysicsBox = PHYSICS.$rigidBody(WHS.Box, PHYSICS.BOX);
 
-var box = new PhysicsBox({
+const box = new PhysicsBox({
   geometry: {
     width: 30,
     height: 2,
@@ -38,16 +30,17 @@ var box = new PhysicsBox({
 
   mass: 0,
 
-  material: _extends({
-    color: UTILS.$colors.mesh
-  }, halfMat),
+  material: {
+    color: UTILS.$colors.mesh,
+    ...halfMat
+  },
 
   position: {
     y: 40
   }
 });
 
-var box2 = new PhysicsBox({
+const box2 = new PhysicsBox({
   geometry: {
     width: 30,
     height: 1,
@@ -56,9 +49,10 @@ var box2 = new PhysicsBox({
 
   mass: 10,
 
-  material: _extends({
-    color: UTILS.$colors.softbody
-  }, halfMat),
+  material: {
+    color: UTILS.$colors.softbody,
+    ...halfMat
+  },
 
   physics: {
     damping: 0.1
@@ -70,14 +64,16 @@ var box2 = new PhysicsBox({
   }
 });
 
-var pointer = new (PHYSICS.$rigidBody(WHS.Sphere, PHYSICS.SPHERE))({ material: { color: UTILS.$colors.mesh, kind: 'phong' } });
+const pointer = new (PHYSICS.$rigidBody(WHS.Sphere, PHYSICS.SPHERE))({material: {color: UTILS.$colors.mesh, kind: 'phong'}});
 pointer.position.set(0, 60, -8);
 pointer.addTo(world);
 
 box.addTo(world);
 box2.addTo(world);
 
-var constraint = new PHYSICS.DOFConstraint(box2.native, box.native, new THREE.Vector3(0, 38, 1));
+const constraint = new PHYSICS.DOFConstraint(box2.native, box.native,
+  new THREE.Vector3(0, 38, 1)
+)
 
 world.addConstraint(constraint);
 constraint.enableAngularMotor(10, 20);
@@ -87,115 +83,3 @@ UTILS.addBasicLights(world);
 
 world.setControls(new WHS.OrbitControls());
 world.start();
-
-},{"./globals":2}],2:[function(require,module,exports){
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.addAmbient = addAmbient;
-exports.addBasicLights = addBasicLights;
-exports.addPlane = addPlane;
-exports.addBoxPlane = addBoxPlane;
-var $world = exports.$world = {
-  stats: "fps", // fps, ms, mb or false if not need.
-  autoresize: "window",
-
-  gravity: [0, -100, 0],
-
-  camera: {
-    position: [0, 10, 50]
-  },
-
-  rendering: {
-    background: {
-      color: 0x162129
-    },
-
-    renderer: {
-      antialias: true
-    }
-  },
-
-  shadowmap: {
-    type: THREE.PCFSoftShadowMap
-  }
-};
-
-var $colors = exports.$colors = {
-  bg: 0x162129,
-  plane: 0x447F8B,
-  mesh: 0xF2F2F2,
-  softbody: 0x434B7F
-};
-
-function addAmbient(world, intensity) {
-  new WHS.AmbientLight({
-    light: {
-      intensity: intensity
-    }
-  }).addTo(world);
-}
-
-function addBasicLights(world) {
-  var intensity = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0.5;
-  var position = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : [0, 10, 10];
-  var distance = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 100;
-
-  addAmbient(world, 1 - intensity);
-
-  return new WHS.PointLight({
-    light: {
-      intensity: intensity,
-      distance: distance
-    },
-
-    shadowmap: {
-      fov: 90
-    },
-
-    position: position
-  }).addTo(world);
-}
-
-function addPlane(world) {
-  var size = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 100;
-
-  return new WHS.Plane({
-    geometry: {
-      width: size,
-      height: size
-    },
-
-    modules: [new PHYSICS.PlaneModule({
-      mass: 0
-    })],
-
-    material: new THREE.MeshPhongMaterial({ color: 0x447F8B }),
-
-    rotation: {
-      x: -Math.PI / 2
-    }
-  }).addTo(world);
-}
-
-function addBoxPlane(world) {
-  var size = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 100;
-
-  return new WHS.Box({
-    geometry: {
-      width: size,
-      height: 1,
-      depth: size
-    },
-
-    modules: [new PHYSICS.BoxModule({
-      mass: 0
-    })],
-
-    material: new THREE.MeshPhongMaterial({ color: 0x447F8B })
-  }).addTo(world);
-}
-
-},{}]},{},[1]);
