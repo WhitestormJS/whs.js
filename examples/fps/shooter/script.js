f157b3f7ca2bd338db76a7f58b64dc7efd7c3965
utrobin <patr.rus@gmail.com>
2017-01-15 02:19:13 +0300

    Update example fps/shooter
    Former-commit-id: b1b05064b3e7eb0de0243c5adfae481c8057739c

--- examples/fps/shooter/script.js
+++ examples/fps/shooter/script.js
@@ -1,55 +1,75 @@
 import * as UTILS from '../../globals';
 
-const world = new (PHYSICS.$world(WHS.World))({
-  ...UTILS.$world,
-
-  physics: {
-    ammo: process.ammoPath
-  },
-
-  camera: {
-    position: {
-      z: 10
-    }
-  }
-});
-
-const sphere = new (PHYSICS.$rigidBody(WHS.Sphere, PHYSICS.SPHERE))({ // Create sphere comonent.
+const sphere = new WHS.Sphere({ // Create sphere comonent.
   geometry: {
     radius: 3,
     widthSegments: 32,
     heightSegments: 32
   },
 
-  mass: 2, // Mass of physics object.
+  modules: [
+    new PHYSICS.SphereModule({
+      mass: 1,
+      restitution: 1
+    })
+  ],
 
-  material: {
-    color: UTILS.$colors.mesh,
-    kind: 'lambert'
-  },
+  material: new THREE.MeshPhongMaterial({
+    color: UTILS.$colors.mesh
+  }),
 
-  position: [0, 100, 0]
+  position: new THREE.Vector3(0, 100, 0)
 });
+
+const world = new WHS.App([
+  new WHS.modules.ElementModule(),
+  new WHS.modules.SceneModule(),
+  new WHS.modules.CameraModule({
+    position: new THREE.Vector3(0, 10, 50)
+  }),
+  new WHS.modules.RenderingModule({
+    bgColor: 0x162129,
+
+    renderer: {
+      antialias: true,
+      shadowmap: {
+        type: THREE.PCFSoftShadowMap
+      }
+    }
+  }),
+  new PHYSICS.WorldModule({
+    ammo: process.ammoPath
+  }),
+  new WHS.FirstPersonControls(sphere, {
+    speed: 500,
+    ypos: -10
+  }),
+  // new WHS.OrbitControlsModule(),
+  new WHS.modules.AutoresizeModule()
+]);
 
 sphere.addTo(world);
 
-const material = new THREE.MeshPhongMaterial({color: UTILS.$colors.mesh});
-
-const PhysicsBox = PHYSICS.$rigidBody(WHS.Box, PHYSICS.BOX);
-
 for (let i = 0; i < 10; i++) {
-  new PhysicsBox({
-    geometry: [10 + Math.random() * 90, 10 + Math.random() * 90, 10 + Math.random() * 90],
-    material,
-    position: [Math.random() * 1000 - 500, 100, Math.random() * 1000 - 500]
+  new WHS.Box({
+    geometry: {
+      width: 10 + Math.random() * 90,
+      height: 10 + Math.random() * 90,
+      depth: 10 + Math.random() * 90
+    },
+
+    material: new THREE.MeshPhongMaterial({color: UTILS.$colors.mesh}),
+
+    position: {
+      x: Math.random() * 1000 - 500,
+      y: 0,
+      z: Math.random() * 1000 - 500
+    }
   }).addTo(world);
 }
 
 UTILS.addPlane(world, 1000);
 UTILS.addBasicLights(world);
 
-world.start(); // Start animations and physics simulation.
-world.setControls(new WHS.FirstPersonControls(sphere, {
-  speed: 3,
-  ypos: -10
-}));
+
+world.start();
