6fcd007b5d28fc0ad80cccb562d5d0c21e4ffa85
sasha240100 <alexbuzin88@gmail.com>
2017-01-15 20:12:13 +0200

    Fix webpack config
    Former-commit-id: 3021472880fd55f3c76d0799cdc69ff72c6ab179

--- src/core/App.js
+++ src/core/App.js
@@ -1,87 +1,6 @@
 import {ModuleManager} from './ModuleManager';
 
 class App {
-  // static defaults = {
-  //   stats: false,
-  //   autoresize: false,
-  //   softbody: false,
-  //
-  //   helpers: {
-  //     grid: false,
-  //     axis: false
-  //   },
-  //
-  //   gravity: {
-  //     x: 0,
-  //     y: 0,
-  //     z: 0
-  //   },
-  //
-  //   rendering: {
-  //     shadowmap: {
-  //       enabled: true,
-  //       type: PCFSoftShadowMap
-  //     },
-  //
-  //     background: {
-  //       color: 0x000000,
-  //       opacity: 1
-  //     },
-  //
-  //     renderer: {}
-  //   },
-  //
-  //   camera: {
-  //     fov: 75,
-  //     near: 1,
-  //     far: 1000,
-  //
-  //     position: {
-  //       x: 0,
-  //       y: 0,
-  //       z: 0
-  //     }
-  //   },
-  //
-  //   width: window.innerWidth,
-  //   height: window.innerHeight,
-  //   container: window.document.body,
-  //
-  //   resolution: {
-  //     width: 1,
-  //     height: 1
-  //   },
-  //
-  //   physics: {
-  //     create: false,
-  //     fixedTimeStep: 1 / 60,
-  //     broadphase: {type: 'dynamic'}
-  //   },
-  //
-  //   modules: []
-  // };
-  //
-  // static instructions = {
-  //   camera: {
-  //     position: ['x', 'y', 'z']
-  //   },
-  //
-  //   gravity: ['x', 'y', 'z']
-  // };
-  //
-  // static helpers = {
-  //   axis: [AxisHelper, {
-  //     size: 5
-  //   }, ['size']],
-  //
-  //   grid: [GridHelper, {
-  //     size: 10,
-  //     step: 1,
-  //     color1: 0xffffff,
-  //     color2: 0xffffff
-  //   }, ['size', 'step', 'color1', 'color2']]
-  // };
-
   simulate = false;
   render = true;
   loops = [];
@@ -89,10 +8,6 @@ class App {
   constructor(modules = []) {
     this.modules = modules;
     this.manager = new ModuleManager(this);
-
-    if (process.env.WHSDEV) {
-      window.appInstance = this;
-    }
 
     for (let i = 0, max = modules.length; i < max; i++) {
       const module = modules[i];
@@ -110,28 +25,6 @@ class App {
     this.manager.reset();
   }
 
-  /**
-   * Start animation.
-   */
-  // start() {
-  //   this.clock = new THREE.Clock();
-
-  //   window.requestAnimFrame = (() => {
-  //     return window.requestAnimationFrame
-  //       || window.webkitRequestAnimationFrame
-  //       || window.mozRequestAnimationFrame
-  //       || function (callback) {
-  //         window.setTimeout(callback, 1000 / 60);
-  //       };
-  //   })();
-
-  //   this.onStartRendering = onStartRendering;
-  //   this.onFinishRendering = onFinishRendering;
-  //   this.$rendering.render(this.$scene, this.$camera.native);
-
-  //   if (this.$rendering) this.$rendering.start(this.beforeRender.bind(this), this.afterRender.bind(this));
-  // }
-
   start() {
     window.requestAnimFrame = (() => {
       return window.requestAnimationFrame
@@ -145,16 +38,10 @@ class App {
     function process() {
       window.requestAnimFrame(process.bind(this));
 
-      // Init stats.
-      // if (stats) stats.begin();
-
       for (let i = 0; i < this.loops.length; i++) {
         const e = this.loops[i];
         if (e.enabled) e.execute(e.clock);
       }
-
-      // End helper.
-      // if (stats) stats.end();
     }
 
     (process.bind(this))();
@@ -173,36 +60,6 @@ class App {
 
     return this;
   }
-
-  // /**
-  //  * Callback called immediately before Plugin Rendering.
-  //  * @param  {Number} delta : delta time elapsed since the last frame.
-  //  */
-  // beforeRender(delta) {
-  //   if (this.controls) {
-  //     this.controls.update(Date.now() - this.time);
-  //     this.time = Date.now();
-  //   };
-  // }
-
-  // *
-  //  * Callback called immediately after the Plugin Rendering.
-  //  * @param  {Number} delta : delta time elapsed since the last frame (will be equal to beforeRender delta).
-
-  // afterRender(delta) {
-  //   for (let i = 0; i < this.loops.length; i++) {
-  //     const e = this.loops[i];
-  //     if (e.enabled) e.execute(e.clock);
-  //   }
-  // }
-
-  /**
-   * Retrieve the renderer used by the active rendering plugin.
-   * @return {THREE.WebGLRenderer} The WebGLRenderer used by the current rendering plugin.
-   */
-  // get renderer() {
-  //   if (this.$rendering) return this.$rendering.$renderer;
-  // }
 
   addLoop(loop) {
     return new Promise((resolve) => {
@@ -232,26 +89,6 @@ class App {
 
     return bridgeMap;
   }
-
-  // addHelper(name, params = {}, helpers = App.helpers) {
-  //   super.addHelper(name, params, helpers);
-  // }
-
-  // addConstraint(constraint) {
-  //   this.$scene.addConstraint(constraint);
-  // }
-
-  /**
-   * This functon will scene properties when it's called.
-   */
-  // setSize(width = 1, height = 1) {
-  //   this.$camera.native.aspect = width / height;
-  //   this.$camera.native.updateProjectionMatrix();
-
-  //   if (this.$rendering) {
-  //     this.$rendering.setSize(width, height);
-  //   }
-  // }
 }
 
 export {
