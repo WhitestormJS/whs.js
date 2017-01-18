b2e78a6bfebe15cca34091520ec9c31cf68f8177
Alexander Buzin <alexbuzin88@gmail.com>
2017-01-18 16:02:15 +0200

    Fix constraints/* examples

--- examples/constraints/slider/script.js
+++ examples/constraints/slider/script.js
@@ -1,62 +1,58 @@
 import * as UTILS from '../../globals';
 
-const world = new (PHYSICS.$world(WHS.World))({
-  ...UTILS.$world,
-
-  physics: {
-    ammo: process.ammoPath
-  },
-
-  camera: {
-    far: 10000,
-    position: [0, 10, 100]
-  }
-});
+const world = new WHS.App([
+  ...UTILS.appModules({
+    position: new THREE.Vector3(0, 40, 70)
+  })
+]);
 
 const halfMat = {
-  kind: 'phong',
   transparent: true,
   opacity: 0.5
 };
 
-const PhysicsBox = PHYSICS.$rigidBody(WHS.Box, PHYSICS.BOX);
-
-const box = new PhysicsBox({
+const box = new WHS.Box({
   geometry: {
     width: 2,
     height: 30,
     depth: 2
   },
 
-  mass: 1,
+  modules: [
+    new PHYSICS.BoxModule({
+      mass: 10
+    })
+  ],
 
-  material: {
+  material: new THREE.MeshPhongMaterial({
     color: UTILS.$colors.mesh,
     ...halfMat
-  },
+  }),
 
   position: {
     y: 40
   }
 });
 
-const box2 = new PhysicsBox({
+const box2 = new WHS.Box({
   geometry: {
     width: 2,
     height: 20,
     depth: 2
   },
 
-  mass: 1,
 
-  material: {
-    color: UTILS.$colors.mesh,
+  modules: [
+    new PHYSICS.BoxModule({
+      mass: 10,
+      damping: 0.1
+    })
+  ],
+
+  material: new THREE.MeshPhongMaterial({
+    color: UTILS.$colors.softbody,
     ...halfMat
-  },
-
-  physics: {
-    damping: 0.1
-  },
+  }),
 
   position: {
     y: 42,
@@ -65,17 +61,16 @@ const box2 = new PhysicsBox({
 });
 
 box.addTo(world);
-box2.addTo(world);
+box2.addTo(world).then(() => {
+  const constraint = new PHYSICS.SliderConstraint(box2, box,
+    new THREE.Vector3(0, box2.position.y, 0),
+    new THREE.Vector3(0, 1, 0)
+  );
 
-const constraint = new PHYSICS.SliderConstraint(box2.native, box.native,
-  new THREE.Vector3(0, box2.position.y, 0),
-  new THREE.Vector3(0, 1, 0)
-);
-
-world.addConstraint(constraint);
+  world.addConstraint(constraint);
+});
 
 UTILS.addPlane(world, 250);
 UTILS.addBasicLights(world);
 
 world.start();
-world.setControls(new WHS.OrbitControls());
