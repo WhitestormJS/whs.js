5ab6e0f161108b16ded4da2c4184348db89513c4
Alexander Buzin <alexbuzin88@gmail.com>
2017-01-25 21:56:28 +0200

    Add alias for webpack projects
    Former-commit-id: 0fb79dc35b02c0f45ac822996214a34ad8711718

---
+++ tools/alias.js
@@ -0,0 +1,23 @@
+module.exports = {
+  // Modules:App
+
+  '@whs:app/element': 'whs/src/modules/app/ElementModule',
+  '@whs:app/scene': 'whs/src/modules/app/SceneModule',
+  '@whs:app/camera': 'whs/src/modules/app/CameraModule',
+  '@whs:app/rendering': 'whs/src/modules/app/RenderingModule',
+
+  // Modules:Controls
+
+  '@whs:controls/orbit': 'whs/src/modules/controls/OrbitModule',
+  '@whs:controls/firstperson': 'whs/src/modules/controls/FirstPersonModule',
+
+  '@whs$': 'whs/src/index',
+  '@whs^three': 'whs/src/three.js',
+  '@whs/core$': 'whs/src/core/index',
+  '@whs/core': 'whs/src/core',
+  '@whs:app$': 'whs/src/modules/app/export',
+  '@whs:app': 'whs/src/modules/app',
+  '@whs:controls$': 'whs/src/modules/controls/export',
+  '@whs:controls': 'whs/src/modules/controls',
+  '@whs+meshes': 'whs/src/components/meshes'
+};
