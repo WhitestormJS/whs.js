4fc73f0493a0c2ae5a82b462d3799f10eb4a77f5
Alexander Buzin <alexbuzin88@gmail.com>
2017-01-25 15:04:08 +0200

    Add ModuleSystem
    Former-commit-id: 47a960d89f105f6d013837b3ab4ba81399ededcc

---
+++ src/core/ModuleSystem.js
@@ -0,0 +1,62 @@
+import Events from 'minivents';
+
+import {ManagerError} from './errors';
+
+export class ModuleSystem extends Events {
+  // INTEGRATING
+
+  integrateModules(source) {
+    if (source) this.modules = source.modules.slice(0);
+
+    for (let i = 0, max = this.modules.length; i < max; i++)
+      this.applyModule(this.modules[i]);
+
+    if (source) this.applyBridge({onCopy: source});
+  }
+
+  // APPLYING MODULE (...and a "bridge" for module)
+
+  applyBridge(bridgeMap = {}) {
+    const modules = this.modules;
+
+    for (let i = 0, max = modules.length; i < max; i++) {
+      for (const key in bridgeMap) {
+        if (bridgeMap[key]) {
+          const module = modules[i];
+
+          if (module.bridge && module.bridge[key])
+            bridgeMap[key] = module.bridge[key].apply(this, [bridgeMap[key], module]);
+        }
+      }
+    }
+
+    return bridgeMap;
+  }
+
+  applyModule(module) {
+    if (!module) return;
+    this.modules.push(module);
+
+    if (this.manager) this.manager.setActiveModule(module);
+
+    if (module.manager && this.manager) module.manager(this.manager);
+    else if (module.manager) {
+      throw new ManagerError(
+        'Component',
+        `Module requires ModuleManager that is turned off for this component`,
+        this, module
+      );
+    }
+
+    if (module.integrate) module.integrate.bind(this)(module.params, module);
+
+    return module;
+  }
+
+  // PIPED METHOD
+
+  module(module) {
+    this.applyModule(module);
+    return this;
+  }
+}
