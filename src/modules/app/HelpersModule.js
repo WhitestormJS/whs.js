150ec6ce5594acc491501f7a3b77a0a2a14e478c
Alexander Buzin <alexbuzin88@gmail.com>
2017-01-27 00:31:07 +0200

    Remove old, unused classes
    Former-commit-id: 9de101be5440584aa36438c6dfd63dfa8560bd6a

--- src/modules/app/HelpersModule.js
+++
@@ -1,8 +0,0 @@
-export class HelpersModule {
-  integrate() {
-    const _helpers = this.params.helpers;
-
-    if (_helpers.axis) this.addHelper('axis', _helpers.axis);
-    if (_helpers.grid) this.addHelper('grid', _helpers.grid);
-  }
-}
