943f19e6ecbeddb4e29a7d7811ae8d30dd543459
Plusb Preco <plusb21@gmail.com>
2017-02-04 20:21:28 +0900

    Rewrite tests into Jest
    Former-commit-id: b62f09c82dd9de702e1b777df745718f851dd7b9

---
+++ test/core/Component.spec.js
@@ -0,0 +1,57 @@
+import * as WHS from '../build/whitestorm';
+
+/*
+ * Ignored methods:
+ *
+ * - .integrateModules() - Does most of .applyModule(), used only in core;
+ * - .modules() - Piped version of .applyModule();
+ * - .add() - Component-specific method that relies on .native;
+ *
+ */
+
+const app = new WHS.App();
+const component = new WHS.Component();
+const component2 = new WHS.Component();
+
+app.start();
+
+test('.wait() & .defer()', (done) => {
+  const promise = new Promise((resolve) => {
+    setTimeout(() => resolve(), 1);
+  });
+
+  component.wait(promise);
+  component.defer(() => done());
+});
+
+test('throws error when .manager is used, but not defined', () => {
+  expect(() => new WHS.Component({manager: false}).manager)
+    .toThrowError('@Component: ModuleManager is not used in this component. \'manager\' parameter should be set as \'true\'');
+});
+
+test('.updateParams()', () => {
+  component.updateParams({
+    hello: 'world'
+  });
+
+  expect(component.params.hello).toBe('world');
+});
+
+test('.copy()', () => {
+  component.copy(component2);
+  expect(component.params).toEqual(component2.params);
+});
+
+test('.clone()', () => {
+  const cloned = component.clone();
+  expect(cloned).not.toEqual(component);
+});
+
+test('.applyModule()', () => {
+  const module = new WHS.mesh.DynamicGeometryModule();
+  expect(component.applyModule(module)).toBe(module);
+});
+
+test('.applyBridge()', () => {
+  expect(component.applyBridge({hello: 'world'}).hello).toBe('world');
+});
