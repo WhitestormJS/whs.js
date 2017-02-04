6ad302404567453fdcafcf18a816ed6b0f5cfe6c
Alexander Buzin <alexbuzin88@gmail.com>
2017-02-04 19:17:30 +0200

    Improve tests coverage

--- test/modules.spec.js
+++ test/modules.spec.js
@@ -26,11 +26,11 @@ test('Applying basic modules to app', () => {
 
 test('OrbitModule', () => {
   modules.orbit = new WHS.controls.OrbitModule();
-})
+});
 
 test('FirstPersonModule', () => {
   modules.fps = new WHS.controls.FirstPersonModule();
-})
+});
 
 // TODO: fix applying controls
 // test('Applying controls modules to app', () => {
@@ -39,4 +39,25 @@ test('FirstPersonModule', () => {
 //     .module(modules.fps);
 //
 //   app.start();
+// });
+
+test('DynamicGeometryModule', () => {
+  modules.dym = new WHS.mesh.DynamicGeometryModule();
+});
+
+test('TextureModule', () => {
+  modules.texture = new WHS.mesh.TextureModule({
+    url: 'myurl.png'
+  });
+});
+
+// TODO: fix applying modules to mesh
+// test('Applying modules to mesh', () => {
+//   const element = new WHS.Sphere();
+//
+//   element
+//     .module(modules.dym)
+//     .module(modules.texture);
+//
+//   element.start();
 // });
