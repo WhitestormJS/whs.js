00d63cc49306669c8b8988480a85cddefdf663f3
Hirako2000 <hirako2000@gmail.com>
2017-03-08 19:40:36 +0700

    #166 - Adds point light example with 3 lights moving around box
    Former-commit-id: 1e8df00abb43f4c25dbc05419784f9455a56906c

---
+++ examples/lights/point/script.js
@@ -0,0 +1,133 @@
+import * as UTILS from '../../globals';
+
+const red = 0xff0040;
+const blue = 0x6666ff;
+const white = 0xffffff;
+const lightIntensity = 1;
+
+const cameraModule = new WHS.app.CameraModule({
+  position: {
+    z: 30,
+    y: 40
+  },
+  far: 20000,
+  near: 1
+});
+
+const controlsModule = new WHS.controls.OrbitModule();
+
+const world = new WHS.App([
+  ...UTILS.appModules({
+    position: new THREE.Vector3(0, 10, 200)
+  }),
+  controlsModule,
+  cameraModule
+]);
+controlsModule.controls.autoRotate = true;
+
+new WHS.Box({
+  geometry: {
+    width: 10,
+    height: 10,
+    depth: 10
+  },
+  material: new THREE.MeshPhongMaterial(),
+  modules: [
+    new WHS.mesh.TextureModule({
+      url: `${process.assetsPath}/textures/box.jpg`
+    })
+  ],
+  position: [0, 5, 0]
+}).addTo(world);
+
+new WHS.Box({
+  geometry: {
+    width: 2000,
+    height: 2000
+  },
+  position: [0, -1.5, 0],
+  rotation: {
+    y: 0,
+    x: -Math.PI / 2
+  },
+  receiveShadow: true,
+  material: new THREE.MeshPhongMaterial({
+    side: THREE.DoubleSide
+  })
+}).addTo(world);
+
+new WHS.AmbientLight({
+  light: {
+    color: 0xffffff,
+    intensity: 0.05
+  }
+}).addTo(world);
+
+const redSpotLight = new WHS.PointLight({
+  light: {
+    color: red,
+    intensity: lightIntensity,
+    distance: 40
+  },
+
+  material: new THREE.MeshBasicMaterial({
+    color: red
+  })
+});
+redSpotLight.addTo(world);
+
+new WHS.Sphere({
+  material: new THREE.MeshBasicMaterial({
+    color: red
+  })
+}).addTo(redSpotLight);
+
+const whiteSpotLight = new WHS.PointLight({
+  light: {
+    color: white,
+    intensity: lightIntensity,
+    distance: 90
+  }
+});
+whiteSpotLight.addTo(world);
+
+new WHS.Sphere({
+  material: new THREE.MeshBasicMaterial({
+    color: white
+  })
+}).addTo(whiteSpotLight);
+
+const blueSpotLight = new WHS.PointLight({
+  light: {
+    color: blue,
+    intensity: lightIntensity,
+    distance: 50
+  }
+});
+blueSpotLight.addTo(world);
+
+new WHS.Sphere({
+  material: new THREE.MeshBasicMaterial({
+    color: blue
+  }),
+  castShadow: false
+}).addTo(blueSpotLight);
+
+const distance = 15;
+const slowFactor = 0.8;
+const fastFactor = 1.4;
+new WHS.Loop(() => {
+  const t = Date.now() * 0.0015;
+  // blue light stays flat on Y axis
+  blueSpotLight.position.x = distance * Math.sin(t * slowFactor);
+  blueSpotLight.position.z = distance * Math.cos(t * slowFactor);
+  // red & white lights bounce on Y axis
+  redSpotLight.position.x = distance * Math.cos(t * fastFactor);
+  redSpotLight.position.y = Math.abs(distance * Math.sin(t * fastFactor));
+  // white is like red, but oscilates its Z as well
+  whiteSpotLight.position.x = distance * Math.sin(t);
+  whiteSpotLight.position.y = Math.abs(distance * Math.cos(t));
+  whiteSpotLight.position.z = distance * Math.cos(t);
+}).start(world);
+
+world.start();
