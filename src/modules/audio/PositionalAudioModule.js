10065a58f99acf119614cf833ee77f08b30fcdc2
Hirako2000 <hirako2000@gmail.com>
2017-03-10 02:26:49 +0700

    #77 - cleanup: new lines and removes forgotten console log
    Former-commit-id: 8c7a4c2f21987f2ef264c5cd99fa79a7ae7b160f

--- src/modules/audio/PositionalAudioModule.js
+++ src/modules/audio/PositionalAudioModule.js
@@ -9,20 +9,21 @@ export class PositionalAudioModule {
     this.params = Object.assign({
       auto: true
     }, params);
+
     this.audioListener = new AudioListener();
     this.positionalAudio = new PositionalAudio(this.audioListener);
     this.audioLoader = new AudioLoader();
   }
 
-  integrate(self) { // self - module scope
+  integrate(self) {
     this.addAudioListener = function (cameraModule) {
-      console.log('adding audio listener of this component');
       this.native.add(self.positionalAudio);
       cameraModule.camera.native.add(self.audioListener);
     };
 
     this.playAudio = function (path) {
       const sound = self.positionalAudio;
+
       self.audioLoader.load(path, buffer => {
         sound.setBuffer(buffer);
         sound.setRefDistance(50);
