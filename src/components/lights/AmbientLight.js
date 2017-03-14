0c361653e4eeaaf239405218cf9fd6f24511305b
Estuardo Diaz <esjoando555@gmail.com>
2017-03-13 23:27:43 -0600

    Sets defaults to Ambient light

--- src/components/lights/AmbientLight.js
+++ src/components/lights/AmbientLight.js
@@ -2,6 +2,15 @@ import {AmbientLight as AmbientLightNative} from 'three';
 import {LightComponent} from '../../core/LightComponent';
 
 class AmbientLight extends LightComponent {
+  static defaults = {
+    ...LightComponent.defaults,
+
+    light: {
+      color: 0xffffff,
+      intensity: 1
+    }
+  };
+
   build(params = {}) {
     return new AmbientLightNative(
       params.light.color,
