7a1357968e853850d49f1051088e8d298ec1f96b
Estuardo Diaz <esjoando555@gmail.com>
2017-03-13 23:29:10 -0600

    Updates defaults for spot light

--- src/components/lights/SpotLight.js
+++ src/components/lights/SpotLight.js
@@ -2,6 +2,19 @@ import {SpotLight as SpotLightNative, SpotLightHelper} from 'three';
 import {LightComponent} from '../../core/LightComponent';
 
 class SpotLight extends LightComponent {
+  static defaults = {
+    ...LightComponent.defaults,
+
+    light: {
+      color: 0xffffff,
+      intensity: 1,
+      distance: 100,
+      angle: Math.PI / 3,
+      exponent: 0,
+      decay: 1
+    }
+  };
+
   static helpers = {
     default: [SpotLightHelper]
   };
