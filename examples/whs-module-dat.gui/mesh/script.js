9daf93506ba08380479f914625c6e01db158151e
Alexander Buzin <alexbuzin88@gmail.com>
2017-03-16 01:48:31 +0200

    Extend dat.gui example
    Former-commit-id: ed3c53a4d93c382907d5eac5179f7c49aed6d1c7

--- examples/whs-module-dat.gui/mesh/script.js
+++ examples/whs-module-dat.gui/mesh/script.js
@@ -1,3 +1,5 @@
+const DatGUI = new DatGUIModule();
+
 const app = new WHS.App(
   new WHS.BasicAppPreset({
     camera: {
@@ -14,39 +16,47 @@ const app = new WHS.App(
   .get()
 );
 
+app.$camera.applyModule(DatGUI.Camera({
+  name: 'camera'
+}));
+
 const sphere = new WHS.Sphere({
-  material: new THREE.MeshBasicMaterial({color: 0xffffff}),
+  material: new THREE.MeshLambertMaterial({color: 0xffffff}),
   modules: [
     new WHS.mesh.DynamicGeometryModule(),
-    new DatGUIModule({
-      name: 'MySphere',
-      material: true,
-      geometry: true,
-      tryMaterial: [
-        THREE.MeshBasicMaterial,
-        THREE.MeshLambertMaterial,
-        THREE.MeshPhongMaterial
-      ],
-      custom: {
-        hello: value => {
-          console.log(value);
+    DatGUI.Mesh({
+      name: 'Sphere1',
+      geometry: {
+        radius: {
+          range: [10, 100]
         }
-      },
-      defaults: {
-        hello: 1
-      },
-      range: {
-        hello: [0, 10]
-      },
-      step: {
-        hello: 2
       }
     })
   ]
 });
 
 new WHS.SpotLight({
-  position: [10, 10, 10]
+  position: [10, 20, 10],
+
+  light: {
+    distance: 200,
+    intensity: 2
+  },
+
+  modules: [
+    DatGUI.Light({
+      name: 'light1'
+    })
+  ]
+}).addTo(app);
+
+window.sp = sphere;
+
+new WHS.Plane({
+  geometry: [100, 100],
+  material: new THREE.MeshLambertMaterial({color: 0xffffff}),
+  position: [0, -10, 0],
+  rotation: [-Math.PI / 2, 0, 0]
 }).addTo(app);
 
 sphere.addTo(app);
