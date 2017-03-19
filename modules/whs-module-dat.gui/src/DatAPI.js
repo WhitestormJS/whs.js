f6ec0afe1b5aba7fdf25f2f7df9c73214a3280bc
Alexander Buzin <alexbuzin88@gmail.com>
2017-03-19 13:38:20 +0200

    Add transforms for dat.gui
    Former-commit-id: 4cd609a02907fd90ca0b9fb94930efec97545ed8

--- modules/whs-module-dat.gui/src/DatAPI.js
+++ modules/whs-module-dat.gui/src/DatAPI.js
@@ -19,4 +19,29 @@ export class DatAPI {
       }
     }
   }
+
+  guiTransforms(native, instance = this.fold) {
+    if (!this.params.transforms) return;
+
+    const controller = instance.addFolder('transforms');
+
+    // position
+    const position = controller.addFolder('position');
+    position.add(native.position, 'x');
+    position.add(native.position, 'y');
+    position.add(native.position, 'z');
+
+    // rotation
+    const rotation = controller.addFolder('rotation');
+    rotation.add(native.rotation, 'x').step(0.1);
+    rotation.add(native.rotation, 'y').step(0.1);
+    rotation.add(native.rotation, 'z').step(0.1);
+
+    // scale
+    if (!native.scale) return;
+    const scale = controller.addFolder('scale');
+    scale.add(native.scale, 'x').step(0.1);
+    scale.add(native.scale, 'y').step(0.1);
+    scale.add(native.scale, 'z').step(0.1);
+  }
 }
