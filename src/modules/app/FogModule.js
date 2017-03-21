696d0f5e67292be575841db57ad50bbbe4bb3bf2
Hirako2000 <hirako2000@gmail.com>
2017-03-21 17:33:29 +0800

    Add support for the two types of three.js Fogs. #207

--- src/modules/app/FogModule.js
+++ src/modules/app/FogModule.js
@@ -1,17 +1,18 @@
 import {
-  FogExp2
+  FogExp2,
+  Fog
 } from 'three';
 
 export class FogModule {
-  constructor(params = {}) {
+  constructor(params = {}, type) {
     this.params = Object.assign({
       color: 0xefd1b5,
       density: 0.020,
-      near: 1,
-      far: 100
+      near: 10,
+      far: 1000
     }, params);
-
-    this.fog = new FogExp2(this.params.color, this.params.density);
+    if (!type || type === 'exp2') this.fog = new FogExp2(this.params.color, this.params.density);
+    else if (type === 'linear') this.fog = new Fog(this.params.color, this.params.near, this.params.far);
   }
 
   manager(manager) {
