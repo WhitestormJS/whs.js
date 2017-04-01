25605ef03ca8b90322aac34a2afd263b7cca043a
Hirako2000 <hirako2000@gmail.com>
2017-04-01 16:42:48 +0100

    Adds further test for the Line constructor #217
    Former-commit-id: 59f59b1464b9883dd2377bf5d655adbcb4f618e8

--- src/components/__tests__/line.spec.js
+++ src/components/__tests__/line.spec.js
@@ -1,4 +1,5 @@
 import * as WHS from '../../index';
+import {Vector3} from 'three';
 
 describe('Line', () => {
   test('constructs with defaults vertices', () => {
@@ -8,7 +9,26 @@ describe('Line', () => {
     expect(line.geometry.vertices[1]).toEqual({"x": 10, "y": 0, "z": 0});
   });
 
-  test('constructs with curve', () => {
-    // TODO, curve needs to be revisted
-  });
+  test('constructs with vertices according to start/end', () => {
+    const startVect = {x: 10, y: 1, z: 15};
+    const endVect = {x: 15, y: 10, z: 25};
+
+    const line = new WHS.Line({
+      geometry: {
+        start: new Vector3(startVect.x, startVect.y, startVect.z),
+        end: new Vector3(endVect.x, endVect.y, endVect.z)
+      }
+    });
+
+    expect(line.geometry.vertices[0]).toEqual({
+      "x": startVect.x,
+      "y": startVect.y,
+      "z": startVect.z
+    });
+    expect(line.geometry.vertices[1]).toEqual({
+      "x":  endVect.x,
+      "y": endVect.y,
+      "z": endVect.z});
+    });
+
 });
