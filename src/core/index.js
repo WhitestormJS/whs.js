199603b618d3f51bf4c1d9747570220cd9d1ef94
Alexander Buzin <alexbuzin88@gmail.com>
2017-04-08 12:38:35 +0300

    wip
    Former-commit-id: 44fec5cbc037cee359d7d98aa371cbcb6435f6f2

--- src/core/index.js
+++ src/core/index.js
@@ -3,5 +3,6 @@ export * from './MeshComponent';
 export * from './LightComponent';
 export * from './CameraComponent';
 export * from './App';
+export * from './TestApp';
 export * from './Loop';
 export * from './ModuleManager';
