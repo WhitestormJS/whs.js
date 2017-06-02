8937c2459de23f876b7a52ae0ae0dc2666187e19
Hirako2000 <hirako2000@gmail.com>
2017-06-02 19:13:53 +0700

    Moves types from DT fork - Adds typescript and tslint to deps #189
    Former-commit-id: 8138f59a120350a16223ff0d1ab586564a9b427b

---
+++ types/core/errors.d.ts
@@ -0,0 +1,17 @@
+/**
+ * TODO
+ */
+export class CompositionError extends Error {
+}
+
+/**
+ * TODO
+ */
+export class DependencyError extends Error {
+}
+
+/**
+ * TODO
+ */
+export class ManagerError extends Error {
+}
