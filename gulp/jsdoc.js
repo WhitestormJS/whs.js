9a466c229b251d01dcdd7f5be360618d1d7eb199
Hirako2000 <hirako2000@gmail.com>
2017-06-14 03:29:40 +0700

    Renames gulp task to build doc #189
    Former-commit-id: 434d17da8ad2bf6c877ab960b3d988737277be46

--- gulp/jsdoc.js
+++ gulp/jsdoc.js
@@ -1,7 +1,7 @@
 import jsdoc from 'gulp-jsdoc3';
 import gulp from 'gulp';
 
-gulp.task('build', function (cb) {
+gulp.task('doc:build', function (cb) {
     const config = require('../jsdoc.json');
     gulp.src(['README.md', './src/**/*.js'], {read: false})
       .pipe(jsdoc(config, cb));
