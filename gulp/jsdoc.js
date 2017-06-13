d1d8b36c965baf4fd07021e10f651b3e52516fed
Hirako2000 <hirako2000@gmail.com>
2017-06-14 03:51:00 +0700

    Adds minami jsdoc theme, cleans up gulp doc build task #189

--- gulp/jsdoc.js
+++ gulp/jsdoc.js
@@ -1,8 +1,8 @@
 import jsdoc from 'gulp-jsdoc3';
 import gulp from 'gulp';
+import config from '../jsdoc';
 
-gulp.task('doc:build', function (cb) {
-    const config = require('../jsdoc.json');
-    gulp.src(['README.md', './src/**/*.js'], {read: false})
-      .pipe(jsdoc(config, cb));
+gulp.task('doc:build', cb => {
+  gulp.src(['README.md', './src/**/*.js'], {read: false})
+    .pipe(jsdoc(config, cb));
 });
