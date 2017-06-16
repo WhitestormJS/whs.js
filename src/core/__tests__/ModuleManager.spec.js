c2409b0658e8ba5c170e20a70b055ed8ae3a1149
Alexander Buzin <alexbuzin88@gmail.com>
2017-06-17 00:09:33 +0300

    Fix tests

--- src/core/__tests__/ModuleManager.spec.js
+++ src/core/__tests__/ModuleManager.spec.js
@@ -1,12 +1,5 @@
 import * as WHS from '../../index';
 
-/*
- * Ignored methods:
- *
- * - .publish() & .unpublish() - Aliases of .add() and .remove();
- *
- */
-
 const composition = {};
 const testValue = {test: true};
 
@@ -18,27 +11,18 @@ test('.active()', () => {
   expect(manager.currentModule).toBe(module);
 });
 
-test('.add()', () => {
-  manager.add('immutable', testValue, {immutable: true, alias: '$testkey'});
-  manager.add('overwritable', testValue);
-
-  expect(manager.store.immutable[0]).toBe(testValue);
-  expect(composition.$testkey).toBe(testValue);
+test('.set()', () => {
+  manager.set('overwritable', testValue);
 });
 
 test('.get()', () => {
-  expect(manager.get('immutable')).toBe(testValue);
+  expect(manager.get('overwritable')).toBe(testValue);
 });
 
 test('.set()', () => {
-  expect(() => {
-    manager.set('immutable', 'newValue');
-  })
-  .toThrowError('@ModuleManager: Dependency \'immutable\' is immutable and already used by another module');
-
   manager.set('overwritable', 'newValue');
 
-  expect(manager.store.overwritable[0]).toBe('newValue');
+  expect(manager.store.getState()[0].overwritable).toBe('newValue');
 });
 
 test('.update()', () => {
@@ -58,16 +42,7 @@ test('.update()', () => {
 });
 
 test('.has()', () => {
-  expect(manager.has('immutable'), 'manager has \'immutable\' dependency').toBeTruthy();
   expect(manager.has('whatever'), 'manager doesn\'t have \'whatever\' dependency').toBeFalsy();
-});
-
-test('.remove()', () => {
-  manager.remove('immutable');
-  manager.remove('overwritable');
-
-  expect(manager.store.immutable).toBe(null);
-  expect(manager.store.overwritable).toBe(null);
 });
 
 test('.reset()', () => {
