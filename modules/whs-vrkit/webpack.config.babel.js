865fae6e5614e46715a13c454137f64d26a91376
Alexander Buzin <alexbuzin88@gmail.com>
2017-06-16 23:29:15 +0300

    Fix readme
    Former-commit-id: e2aeea879af95a157519ffbe25d0e810ea36ad4b

--- modules/whs-vrkit/webpack.config.babel.js
+++ modules/whs-vrkit/webpack.config.babel.js
@@ -12,7 +12,7 @@ console.log(
 );
 
 export default {
-  devtool: isProduction ? false : 'source-map',
+  devtool: isProduction ? '#hidden-source-map' : '#source-map',
   entry: './src/index.js',
   target: 'web',
   output: {
@@ -22,8 +22,18 @@ export default {
     library: 'VRKit'
   },
   externals: {
-    whs: 'WHS',
-    three: 'THREE'
+    three : {
+      commonjs: 'three',
+      commonjs2: 'three',
+      amd: 'three',
+      root: 'THREE' // indicates global variable
+    },
+    whs : {
+      commonjs: 'whs',
+      commonjs2: 'whs',
+      amd: 'whs',
+      root: 'WHS' // indicates global variable
+    }
   },
   module: {
     loaders: [
@@ -38,7 +48,8 @@ export default {
   ? [
     new webpack.optimize.UglifyJsPlugin({
       compress: { warnings: false },
-      minimize: true
+      minimize: true,
+      sourceMap: true
     }),
   ]
   : [],
