3aae1103df63bccaef3fca0635f228b6e1bb6d4e
Alexander Buzin <alexbuzin88@gmail.com>
2017-06-17 23:27:21 +0300

    New design
    Former-commit-id: 80dee6f535d2e2bb61a93319e253c054119ede3a

--- gulp/docs.js
+++ gulp/docs.js
@@ -1,8 +1,38 @@
 import jsdoc from 'gulp-jsdoc3';
 import gulp from 'gulp';
+import watch from 'gulp-watch';
 import config from '../jsdoc';
+import less from 'gulp-less';
 
 gulp.task('docs', cb => {
-  gulp.src(['README.md', './src/**/*.js'], {read: false})
+  gulp.src('./docs/template/static/less/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('./docs/public/styles/'));
+
+  gulp.src(['./docs/data/**/*', './src/core/Component.js']) // ['README.md', './src/**/*.js']
     .pipe(jsdoc(config, cb));
+});
+
+gulp.task('docs:watch', () => {
+  let i = 0;
+
+  watch([
+    './docs/template/**/*.less'
+  ], () => {
+    console.log(`update styles #${i++}`);
+
+    gulp.src('./docs/template/static/less/*.less')
+      .pipe(less())
+      .pipe(gulp.dest('./docs/public/styles/'));
+  });
+
+  return watch([
+    './docs/template/**/*.tmpl',
+    './docs/data/**/*'
+  ], () => {
+    console.log(`update #${i++}`);
+
+    gulp.src(['./docs/data/**/*.md', './src/core/Component.js'])
+      .pipe(jsdoc(config));
+  });
 });
