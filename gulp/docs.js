cc01cecd9e1b12450b12bbc7662ce484f5ad748d
Alexander Buzin <alexbuzin88@gmail.com>
2017-06-17 16:23:06 +0300

    Rename task to docs

--- gulp/docs.js
+++ gulp/docs.js
@@ -2,7 +2,7 @@ import jsdoc from 'gulp-jsdoc3';
 import gulp from 'gulp';
 import config from '../jsdoc';
 
-gulp.task('doc:build', cb => {
+gulp.task('docs', cb => {
   gulp.src(['README.md', './src/**/*.js'], {read: false})
     .pipe(jsdoc(config, cb));
 });
