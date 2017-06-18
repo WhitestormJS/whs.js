a583b213ba0d5cd7b8b42ac8626ad92cd070f30e
Hirako2000 <hirako2000@gmail.com>
2017-06-18 22:25:41 +0700

    Adds some unit tests, import Clock instead of using three.js global #249

---
+++ src/modules/mesh/__tests__/AnimationModule.spec.js
@@ -0,0 +1,41 @@
+import '../../../polyfill';
+import {Clock, AnimationMixer} from 'three';
+import {Box} from '../../../index';
+import {AnimationModule} from '../AnimationModule';
+
+const box = new Box();
+box.skeleton = ['skel'];
+box.geometry = {
+  animations: ['anim']
+};
+
+const modules = {};
+const animationModule = new AnimationModule();
+modules.animationModule = animationModule;
+
+describe('Animation module', () => {
+  test('constructs with a clock', () => {
+    expect(animationModule.clock).toBeInstanceOf(Clock);
+  });
+
+  test('bridges mesh creating an animation mixer', () => {
+    animationModule.bridge.mesh(box, animationModule);
+    expect(animationModule.mixer).toBeInstanceOf(AnimationMixer);
+  });
+
+  test('bridges mesh populating clips from mesh animation', () => {
+    animationModule.bridge.mesh(box, animationModule);
+    expect(animationModule.clips).toEqual(box.geometry.animations);
+  });
+
+  test('bridges mesh passing mesh skeleton to its geometry', () => {
+    animationModule.bridge.mesh(box, animationModule);
+    expect(box.geometry.skeleton).toEqual(box.skeleton);
+  });
+
+  test('applies to mesh', () => {
+    box.module(modules.animationModule);
+  });
+
+  // TODO test play/update (mock?)
+});
