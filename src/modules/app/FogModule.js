9d395b82cd6596e88dfec0f5e3a3cc026cebfeef
Hirako2000 <hirako2000@gmail.com>
2017-06-20 17:31:46 +0700

    Adds FogModule doc - #189
    Former-commit-id: 107f6785ceea0b5eada7c0cf651c110d19f5486d

--- src/modules/app/FogModule.js
+++ src/modules/app/FogModule.js
@@ -1,8 +1,27 @@
+/** @module modules/app/FogModule */
 import {
   FogExp2,
   Fog
 } from 'three';
 
+/**
+ * @class FogModule
+ * @param {Object} [params={color: 0xefd1b5, density: 0.020, near: 10, far: 1000}] - The parameters object.
+ * @param {String} [type=exp2] - The type of fog - exp2 or linear
+ * @memberof module:modules/app
+ * @example <caption>How to reate and apply a FogModule</caption>
+ * const fogModule = new FogModule({
+ *    color: 0xffffff,
+ *    density: 0.03,
+ *    near: 20,
+ *    far: 200
+ *  }, 'exp2');
+ *
+ * new App([
+ *  ...,
+ *  fogModule
+ * ]);
+ */
 export class FogModule {
   constructor(params = {}, type) {
     this.params = Object.assign({
