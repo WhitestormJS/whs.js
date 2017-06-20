e399ebdb3d01a07220a01db54440ba409dffa822
Hirako2000 <hirako2000@gmail.com>
2017-06-21 03:52:42 +0700

    Adds doc for DirectionalLight - #189

--- src/components/lights/DirectionalLight.js
+++ src/components/lights/DirectionalLight.js
@@ -1,6 +1,25 @@
 import {DirectionalLight as DirectionalLightNative, DirectionalLightHelper} from 'three';
 import {LightComponent} from '../../core/LightComponent';
 
+/**
+ * @class DirectionalLight
+ * @description DirectinalLight creates a light that shines from a specific direction not from a specific position.<br/><br/>
+ * This light will behave as though it is infinitely far away and the rays produced from it are all parallel. <br/><br/>
+ * The best analogy would be a light source that acts like the sun: the sun is so far away that all sunlight hitting objects comes from the same angle.<br/><br/>
+ * It has the same options as AmbientLight in light paramater, but it also supports pos and target paramaters.
+ * @param {Object} [params={light: {color: 0xffffff, intensity: 1}}] - The params.
+ * @extends LightComponent
+ * @memberof module:components/lights
+ * @example <caption>Creating a DirectionalLight to fall down from vec3(10, 20, 10) to vec3(0, 0, 0)</caption>
+ * new DirectionalLight({
+ *   light: {
+ *     color: 0xffffff,
+ *     intensity: 0.2
+ *   },
+ *
+ *   position: [10, 20, 10]
+ * }).addTo(app);
+ */
 class DirectionalLight extends LightComponent {
   static defaults = {
     ...LightComponent.defaults,
