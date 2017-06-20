2a2d158ee4ae1b5c0fd8641b412dca1f9b58f659
Hirako2000 <hirako2000@gmail.com>
2017-06-21 02:16:37 +0700

    Adds doc for App component - #189
    Former-commit-id: a6d56ef1be1b0fa5ef9363df36f20ff69a63dd71

--- src/core/App.js
+++ src/core/App.js
@@ -1,10 +1,34 @@
 import {system} from '../polyfill';
 import {ModuleSystem} from './ModuleSystem';
 import {ModuleManager} from './ModuleManager';
-
+/**
+ * @class App
+ * @description This component is used to prepare a world scene, setup physics, camera, renderer and all other things that you usually do before making meshes.
+ * @param {Array} [modules=[]] - Array of Modules
+ * @extends ModuleSystem
+ * @memberof module:core
+ */
 class App extends ModuleSystem {
+  /**
+   * Simulate flag
+   * @description Same as .updateEnabled, but for physics. Defines if physics is simulated each frame.
+   * @member {Boolean} module:core.App#simulate
+   * @public
+   */
   simulate = false;
+
+  /**
+   * @description Defines whether the scene should render or not
+   * @member {Boolean} module:core.App#updateEnabled
+   * @public
+   */
   updateEnabled = true;
+  /**
+   * Loops in this app
+   * @description Array of loops that are executed by this app.
+   * @member {Array} module:core.App#loops
+   * @public
+   */
   loops = [];
 
   constructor(modules = []) {
@@ -17,6 +41,11 @@ class App extends ModuleSystem {
 
   // CONTROLS & UPDATING
 
+  /**
+   * @method start
+   * @description Start rendering loop and physics simulation (if you use version with physics).
+   * @memberof module:core.App
+   */
   start() {
     const requestAnimFrame = (() => {
       return system.window.requestAnimationFrame
@@ -43,10 +72,31 @@ class App extends ModuleSystem {
     process();
   }
 
+  /**
+   * @method stop
+   * @description Stops rendering loops
+   * @memberof module:core.App
+   */
   stop() {
     this.updateEnabled = false;
   }
 
+  /**
+   * @method addLoop
+   * @description Adds loop to this app.
+   * @param {Object} loop - the loop to add
+   * @return {Promise} Promise that is resolved when promises completed.
+   * @memberof module:core.App
+   * @example <caption>Adding a loop to an app</caption>
+   * const loop = new Loop(() => {
+   *  // ...
+   * });
+   *
+   * const app = new App();
+   *
+   * app.addLoop(loop);
+   * loop.start();
+   */
   addLoop(loop) {
     return new Promise(resolve => {
       this.loops.push(loop);
@@ -54,6 +104,13 @@ class App extends ModuleSystem {
     });
   }
 
+  /**
+   * @method removeLoop
+   * @description Removes loop from this app.
+   * @param {Object} loop - the loop to remove
+   * @return {Promise} Promise that is resolved when promises completed.
+   * @memberof module:core.App
+   */
   removeLoop(loop) {
     return new Promise(resolve => {
       this.loops.filter(l => l !== loop);
