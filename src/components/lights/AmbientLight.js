c642034a0adfcd638c357a0c2075c114644bbfe0
Hirako2000 <hirako2000@gmail.com>
2017-06-21 03:46:53 +0700

    Adds doc for AmbientLight - #189
    Former-commit-id: 1fe1004d0de867489c8affe25fe5399bc502b514

--- src/components/lights/AmbientLight.js
+++ src/components/lights/AmbientLight.js
@@ -1,6 +1,22 @@
 import {AmbientLight as AmbientLightNative} from 'three';
 import {LightComponent} from '../../core/LightComponent';
 
+/**
+ * @class AmbientLight
+ * @description AmbientLight is a simple class, it extends Light and inherits all its methods.
+ * AmbientLight creates basic light around all scene, so it doesn't need properties like pos or target.
+ * It supports only color and intensity as parameters, which defines the color of the surrounded light and intensity of light.
+ * @param {Object} [params={light: {color: 0xffffff, intensity: 1}}] - The params.
+ * @extends LightComponent
+ * @memberof module:components/lights
+ * @example <caption>Creating an AmbientLight </caption>
+ * new AmbientLight({
+ *   light: {
+ *     color: 0xffffff,
+ *     intensity: 0.2
+ *   }
+ * }).addTo(world);
+ */
 class AmbientLight extends LightComponent {
   static defaults = {
     ...LightComponent.defaults,
