2e8cff7749fdbf3336dff730ce077de3e3c810f2
Hirako2000 <hirako2000@gmail.com>
2017-06-21 04:06:18 +0700

    Adds doc for PointLight - #189

--- src/components/lights/PointLight.js
+++ src/components/lights/PointLight.js
@@ -1,6 +1,24 @@
 import {PointLight as PointLightNative, PointLightHelper} from 'three';
 import {LightComponent} from '../../core/LightComponent';
 
+/**
+ * @class PointLight
+ * @description PointLight creates a light at a specific position in the scene. The light shines in all directions (roughly similar to a light bulb.)<br/><br/>
+ * It has the same options as AmbientLight in light paramater, but it also supports position, distance and decay.<br/>
+ * @param {Object} [params={light: {color: 0xffffff, intensity: 1, distance: 100, decay: 1}}] - The params.
+ * @extends LightComponent
+ * @memberof module:components/lights
+ * @example <caption>Creating a PointLight</caption>
+ * new LightComponent({
+ *   light: {
+ *     color: 0xff0000,
+ *     intensity: 3,
+ *     distance: 1000
+ *   },
+ *
+ *   position: [10, 20, 10]
+ * }).addTo(app);
+ */
 class PointLight extends LightComponent {
   static defaults= {
     ...LightComponent.defaults,
