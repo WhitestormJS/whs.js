a7dfb6c9602410a2d414eaa02e1318b7a13bd2d8
Hirako2000 <hirako2000@gmail.com>
2017-06-21 03:01:22 +0700

    Adds doc for ModuleSystem - #189
    Former-commit-id: 27e230b70dccb6b2f92aded2e2b75ea13e73d76b

--- src/core/ModuleSystem.js
+++ src/core/ModuleSystem.js
@@ -13,9 +13,23 @@ try {
   warnDeps();
 }
 
+/**
+ * @class ModuleSystem
+ * @description  Provides API for classes that will use Modules.<br/>
+ * This class includes basic event system with those supported methods:
+ * <pre>.on()</pre><pre>.off()</pre><pre>.emit()</pre>
+ * @extends Events
+ * @memberof module:core
+ */
 export class ModuleSystem extends Events {
   // INTEGRATING
 
+  /**
+   * @method integrateModules
+   * @description This method applies all modules from .modules collection.
+   * @param {Object} [source] If source (should be a component) is provided, will replace .modules with source's one before executing modules.
+   * @memberof module:core.ModuleSystem
+   */
   integrateModules(source) {
     if (source) this.modules = source.modules.slice(0);
 
@@ -27,6 +41,13 @@ export class ModuleSystem extends Events {
 
   // APPLYING MODULE (...and a "bridge" for module)
 
+  /**
+   * @method applyBridge
+   * @description Makes component-specific API to work with modules.
+   * @param {Object} bridgeMap
+   * @return {Object} Returns object with modified values.
+   * @memberof module:core.ModuleSystem
+   */
   applyBridge(bridgeMap = {}) {
     const modules = this.modules;
 
@@ -44,6 +65,16 @@ export class ModuleSystem extends Events {
     return bridgeMap;
   }
 
+  /**
+   * @method applyModule
+   * @description .applyModule is also used in .integrateModules() function.
+   * It does exactly what its name says (applies module to component or app).
+   * @param {Object} module the module to apply
+   * @param {Boolean} [push=true]
+   * @return {Object} Returns module that was applied.
+   * @throws {ManagerError}
+   * @memberof module:core.ModuleSystem
+   */
   applyModule(module, push = true) {
     if (!module) return;
     if (push) this.modules.push(module);
@@ -86,6 +117,19 @@ export class ModuleSystem extends Events {
 
   // PIPED METHOD
 
+  /**
+   * @method module
+   * @description piped version of .applyModule().
+   * @param {Object} module the module to apply
+   * @return {Object} returns this - app/component
+   * @throws {ManagerError}
+   * @memberof module:core.ModuleSystem
+   * @example <caption>Piped modules</section>
+   * component
+   *   .module(new Module1())
+   *   .module(new Module2())
+   *   .module(new Module3())
+   */
   module(module) {
     this.applyModule(module);
     return this;
