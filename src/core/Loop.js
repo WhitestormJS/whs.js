4650aebae26776b77b9c02a27e3782b45b4bb35e
Hirako2000 <hirako2000@gmail.com>
2017-06-21 03:39:20 +0700

    Adds doc for Loop - #189
    Former-commit-id: 10e3693395b26e85d4b8fea11a956a8dada5b310

--- src/core/Loop.js
+++ src/core/Loop.js
@@ -1,5 +1,11 @@
 import {Clock} from 'three';
 
+/**
+ * @class Loop
+ * @param {Function} func function to execute on each animation frame
+ * @param {Boolean} [useClock=true] passes a Clock to the function when called, if true
+ * @memberof module:core
+ */
 class Loop {
   constructor(func, useClock = true) {
     this.func = func;
@@ -9,6 +15,12 @@ class Loop {
 
   // CONTROLS
 
+  /**
+   * @method start
+   * @description Starts this loop, clock if it has one. Won't do anything if loop enabled already.
+   * @param {Component} [world] app to add this loop to, if provided.
+   * @memberof module:core.Loop
+   */
   start(world) {
     if (this.enabled) return;
 
@@ -18,6 +30,12 @@ class Loop {
     this.enabled = true;
   }
 
+  /**
+   * @method stop
+   * @description Stops this loop and its clock if it has one, won't do anything if this loop is not enabled)
+   * @param {Component} [world] app to remove this loop from, if provided.
+   * @memberof module:core.Loop
+   */
   stop(world) {
     if (!this.enabled) return;
 
@@ -29,6 +47,12 @@ class Loop {
 
   // EXECUTION
 
+  /**
+   * @method execute
+   * @description Executes the function of this loop
+   * @memberof module:core.Loop
+   * @returns {*} whatever the function of this loop returns
+   */
   execute() {
     return this.func(this.clock);
   }
