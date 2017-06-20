563c85b7c98f3db67d1947768af2f41336169c2b
Hirako2000 <hirako2000@gmail.com>
2017-06-21 03:57:16 +0700

    Adds doc for HemisphereLight - #189
    Former-commit-id: b9b562d5919698d08fd41fecf7d57e5867fe9bf6

--- src/components/lights/HemisphereLight.js
+++ src/components/lights/HemisphereLight.js
@@ -1,6 +1,22 @@
 import {HemisphereLight as HemisphereLightNative, HemisphereLightHelper} from 'three';
 import {LightComponent} from '../../core/LightComponent';
 
+/**
+ * @class HemisphereLight
+ * @description HemisphereLight is a light source positioned directly above the scene.<br/>
+ * It also doesn't need position and target properties.
+ * @param {Object} [params={light: {skyColor: 0xffffff, groundColor: 0xffffff, intensity: 1}}] - The params.
+ * @extends LightComponent
+ * @memberof module:components/lights
+ * @example <caption>Creating a HemisphereLight</caption>
+ * new HemisphereLight({
+ *   light: {
+ *     skyColor: 0xff0000,
+ *     groundColor: 0x0000ff,
+ *     intensity: 0.2
+ *   }
+ * }).addTo(app);
+ */
 class HemisphereLight extends LightComponent {
   static defaults = {
     ...LightComponent.defaults,
