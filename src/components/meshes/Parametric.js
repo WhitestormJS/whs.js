2f2e558d72900fff748745072ff74170cfa4f328
Hirako2000 <hirako2000@gmail.com>
2017-06-22 02:23:43 +0700

    Adds doc for Octahedron, Parametric and Plane - #189
    Former-commit-id: 25e25f6fa4aed7717d2ce8308f5a7c73e887e7fb

--- src/components/meshes/Parametric.js
+++ src/components/meshes/Parametric.js
@@ -7,7 +7,53 @@ import {
 
 import {MeshComponent} from '../../core/MeshComponent';
 
+/**
+ * @class Parametric
+ * @category components/meshes
+ * @description `Parametric` generates a geometry representing a <a href='https://en.wikipedia.org/wiki/Parametric_surface'>Parametric surface</a>
+ * <br/><br/>
+ * It is usually used to develop different kinds of highfields or visualize a <a href='https://stemkoski.github.io/Three.js/Graphulus-Function.html'>math function</a>.
+ * <br/>
+ * - <a href='http://math.hws.edu/graphicsbook/source/threejs/curves-and-surfaces.html'>Parametric surface</a>
+ * - <a href='https://stemkoski.github.io/Three.js/Graphulus-Surface.html'>"Graphulus"</a>
+ * <br/><br/>
+ * @param {Object} [params] - The params.
+ * @extends MeshComponent
+ * @memberof module:components/meshes
+ * @example <caption>Example creating an heightfield-like geometry. `u` and `v` are like `x` and `y` in shape, but their values are always from `0` to `1`.
+ * We use them in `THREE.Vector3` like `x` and `z` and `Math.random() * 5` for `y`.</caption>
+ * const createParametric = (u, v) => {
+ *   return new THREE.Vector3(u * 30, Math.random() * 5, v * 30);
+ * }
+ *
+ * new Parametric({
+ *   geometry: {
+ *     func: createParametric
+ *   },
+ *
+ *   material: new THREE.MeshLambertMaterial({
+ *     color: 0xffffff,
+ *     side: THREE.DoubleSide
+ *   }),
+ *
+ *   position: [0, 100, -100]
+ * }).addTo(app);
+ */
 class Parametric extends MeshComponent {
+  /**
+   * Default values for parameters
+   * @member {Object} module:components/meshes.Parametric#defaults
+   * @static
+   * @default <pre>
+   * {
+   *   geometry: {
+   *     func: (u, v) => new Vector3(u, v, 0),
+   *     slices: 10,
+   *     tacks: 10
+   *   }
+   * }
+   * </pre>
+   */
   static defaults = {
     ...MeshComponent.defaults,
     geometry: {
