14b3b35efaa3972063bf6ed5e89ad45b0d60af24
Hirako2000 <hirako2000@gmail.com>
2017-06-21 22:08:18 +0700

    Adds doc for all cameras - #189
    Former-commit-id: 9c7e927be4775ce5c368380eed9cbb13d919d3e9

--- src/components/cameras/PerspectiveCamera.js
+++ src/components/cameras/PerspectiveCamera.js
@@ -4,12 +4,42 @@ import {system} from '../../polyfill';
 
 /**
  * @class PerspectiveCamera
+ * @description Camera with perspective projection.
  * @category components/cameras
  * @param {Object} [params] - The parameters object.
  * @memberof module:components/cameras
  * @extends CameraComponent
+ * @example <caption>Create an PerspectiveCamera and set it as app's camera</caption>
+ * const camera = new PerspectiveCamera({
+ *   camera: {
+ *     fov: 75,
+ *     aspect: window.innerWidth / window.innerHeight
+ *   },
+ *
+ *   position: {
+ *     x: 0,
+ *     y: 100,
+ *     z: 0
+ *   }
+ * });
+ *
+ * app.camera = camera;
  */
 class PerspectiveCamera extends CameraComponent {
+  /**
+   * Default values for parameters
+   * @member {Object} module:components/cameras.PerspectiveCamera#defaults
+   * @static
+   * @default <pre>
+   * {
+   *   camera: {
+   *     near: 1,
+   *     far: 1000,
+   *     fov: 45,
+   *     aspect: system.window.innerWidth / system.window.innerHeight
+   *   }
+   * }</pre>
+   */
   static defaults = {
     ...CameraComponent.defaults,
 
