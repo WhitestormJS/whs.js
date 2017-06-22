4c1e469f9fa365769904bf8fd3ee7dbb030391a0
Alexander Buzin <alexbuzin88@gmail.com>
2017-06-22 13:56:17 +0300

    Remove extras & compact

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 /**
- * WHS.
+ * Namespace containing all classes from all modules. Used as global in UMD pattern.
  * @namespace WHS
+ * @example <caption>The use of WHS namespace.</caption>
+ * new WHS.App() // core
+ * new WHS.PerspectiveCamera() // components
+ * new WHS.ResizeModule() // modules
+ * WHS.extend() // utils
  */
 
 export * from './core/index';
 export * from './components/lights/index';
 export * from './components/cameras/index';
 export * from './components/meshes/index';
-export * from './extras/index';
 export * from './utils/index';
 export * from './modules/app/index';
 export * from './modules/app/controls/index';
