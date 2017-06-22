f82669c45096b87854246377417dbe82ee7aeab4
Hirako2000 <hirako2000@gmail.com>
2017-06-22 21:13:27 +0700

    Adjusts DefineModule example description patch#2
    Former-commit-id: 99f1be6ae78d4d36ca03db40618aebb608fffa3c

--- src/modules/DefineModule.js
+++ src/modules/DefineModule.js
@@ -4,13 +4,12 @@
  * @param {String} name
  * @param {Object} data
  * @memberof module:modules
- * @example <caption> Creating a rendering module and passing it to App's modules</caption>
+ * @example <caption> Creating a DefineModule with PerspectiveCamera as camera module and passing it to App's modules</caption>
  * new App([
  *   // ...
  *   new DefineModule('camera', new PerspectiveCamera())
  * ]);
  */
-
 export class DefineModule {
   constructor(name, data) {
     this.name = name;
