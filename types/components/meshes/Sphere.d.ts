7a842e45c9b355c91a3c16e4a9e20bec0947033d
Hirako2000 <hirako2000@gmail.com>
2017-07-04 14:37:34 +0700

    Completes Box/Sphere definition - Adds buffer flag to MeshComponentParams - removes unused material import #276

--- types/components/meshes/Sphere.d.ts
+++ types/components/meshes/Sphere.d.ts
@@ -1,10 +1,29 @@
 import {MeshComponent, MeshComponentParams} from '../../core/MeshComponent';
 
-interface SphereParameter extends MeshComponentParams {
+interface SphereParams extends MeshComponentParams {
+
+  /** Geometry parameters */
   geometry?: {
+
+      /** 
+       * Radius of the sphere. 
+       * Default is 1. 
+       */
       radius?: number;
+
+      /** 
+       * Number of horizontal segments. 
+       * Minimum value is 3, and the default is 8. 
+       */
       widthSegments?: number;
+
+      /** 
+       * Number of vertical segments. 
+       * Minimum value is 2, and the default is 6. 
+       */
       heightSegments?: number;
+
+
       phiStart?: number;
       phiLength?: number;
       thetaStart?: number;
@@ -12,10 +31,29 @@ interface SphereParameter extends MeshComponentParams {
   }
 }
 
+interface BufferedSphereParams extends SphereParams {
+
+  /** Sets whether to build a buffered geometry */
+  buffer?: boolean
+} 
+
  export class Sphere extends MeshComponent {
 
    /**
+    * @constructor Creates a Sphere
     * @param params
     */
-   constructor(params?: SphereParameter);
+   constructor(params?: SphereParams);
+
+   /**
+    * Build lifecycle creates a mesh using input params.
+    * @param params 
+    */
+   build(params?: SphereParams);
+
+   /**
+    * Build the geometry
+    * @param params 
+    */
+   buildGeometry(params?: BufferedSphereParams);
 }
