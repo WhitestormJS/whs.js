694139eccebaf749788a5bd92e48cb45b1aa460d
Hirako2000 <hirako2000@gmail.com>
2017-07-04 13:57:06 +0700

    Adds types for lights, also improves a few other definitions #271

---
+++ types/components/lights/AmbientLight.d.ts
@@ -0,0 +1,26 @@
+import {
+  LightComponent
+} from '../../core';
+
+interface AmbientLightParams {
+  /** hexadecimal color of the light. Default is 0xffffff (white). */
+  color: number,
+
+  /** Intensity/strength of the light, Default is 1. */
+  intensity: number
+}
+
+export class AmbientLight extends LightComponent {
+
+  /**
+   * @constructor Creates an ambient light
+   * @param params parameters
+   */
+  constructor(params: AmbientLightParams);
+
+  /**
+   * Builds the ambient light
+   */
+  build(): AmbientLight;
+
+}
