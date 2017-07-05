9fa27c8e3e889e176ee178af9cbd23789827b17c
Alexander Buzin <alexbuzin88@gmail.com>
2017-07-05 14:26:19 +0300

    wip: modules-dev
    Former-commit-id: 19219634fc3c7ba9b8334cd2d85fa128c29d935c

---
+++ modules/AudioModule.js
@@ -0,0 +1,76 @@
+/* Built for whs v1.0.0 */
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('three')) :
+	typeof define === 'function' && define.amd ? define(['three'], factory) :
+	(global.AudioModule = factory(global.THREE));
+}(this, (function (three) { 'use strict';
+
+var classCallCheck = function (instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
+  }
+};
+
+var createClass = function () {
+  function defineProperties(target, props) {
+    for (var i = 0; i < props.length; i++) {
+      var descriptor = props[i];
+      descriptor.enumerable = descriptor.enumerable || false;
+      descriptor.configurable = true;
+      if ("value" in descriptor) descriptor.writable = true;
+      Object.defineProperty(target, descriptor.key, descriptor);
+    }
+  }
+
+  return function (Constructor, protoProps, staticProps) {
+    if (protoProps) defineProperties(Constructor.prototype, protoProps);
+    if (staticProps) defineProperties(Constructor, staticProps);
+    return Constructor;
+  };
+}();
+
+var PositionalAudioModule = function () {
+  function PositionalAudioModule() {
+    var params = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+    classCallCheck(this, PositionalAudioModule);
+    this.bridge = {
+      mesh: function mesh(_mesh, self) {
+        _mesh.add(self.positionalAudio);
+        return _mesh;
+      }
+    };
+
+    this.params = Object.assign({
+      loop: true
+    }, params);
+
+    this.audioListener = new three.AudioListener();
+    this.audioLoader = new three.AudioLoader();
+
+    this.positionalAudio = new three.PositionalAudio(this.audioListener);
+    this.positionalAudio.setLoop(this.params.loop);
+  }
+
+  createClass(PositionalAudioModule, [{
+    key: 'addListener',
+    value: function addListener(object) {
+      object.native.add(this.audioListener);
+    }
+  }, {
+    key: 'playAudio',
+    value: function playAudio(path) {
+      var sound = this.positionalAudio;
+
+      this.audioLoader.load(path, function (buffer) {
+        sound.setBuffer(buffer);
+        sound.setRefDistance(50);
+        sound.play();
+      });
+    }
+  }]);
+  return PositionalAudioModule;
+}();
+
+return PositionalAudioModule;
+
+})));
