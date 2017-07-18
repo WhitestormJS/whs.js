aea9bcc4eb1506c414cbfe8c724449df2c779dae
Alexander Buzin <alexbuzin88@gmail.com>
2017-07-18 11:27:26 +0300

    Add docs for ControlsModule & OrbitControlsModule
    Former-commit-id: d59824da4da55f1ad24780c690ef0a2127e6b1a9

--- src/modules/app/ControlsModule.js
+++ src/modules/app/ControlsModule.js
@@ -1,6 +1,23 @@
 import {Loop} from '../../core/Loop';
 import {EventsPatchModule} from './EventsPatchModule';
 
+/**
+ * @class ControlsModule
+ * @category modules/app
+ * @param {Object} [params]
+ * @memberof module:modules/app
+ * @example <caption> Creating a rendering module and passing it to App's modules</caption>
+ * new App([
+ *   new ElementModule(),
+ *   new SceneModule(),
+ *   new DefineModule('camera', new WHS.PerspectiveCamera({
+ *     position: new THREE.Vector3(0, 6, 18),
+ *     far: 10000
+ *   })),
+ *   new RenderingModule(),
+ *   new ControlsModule.from(new THREE.TrackballControls())
+ * ]);
+ */
 export class ControlsModule {
   static from(controls) {
     return new ControlsModule({controls});
@@ -24,11 +41,25 @@ export class ControlsModule {
     manager.require('events', () => new EventsPatchModule());
   }
 
+  /**
+   * @method setControls
+   * @description Set working controls
+   * @param {Object} controls Working three.js controls object.
+   * @return {this}
+   * @memberof module:modules/app.ControlsModule
+   */
   setControls(controls) {
     this.controls = controls;
     return this;
   }
 
+  /**
+   * @method setUpdate
+   * @description Set controsl update function
+   * @param {Function} update Update function
+   * @return {this}
+   * @memberof module:modules/app.ControlsModule
+   */
   setUpdate(update) {
     this.update = update;
     return this;
