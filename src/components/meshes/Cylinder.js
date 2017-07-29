97030c8f141207c5ca3f0ec9d9be8a3b2bd6b393
Hirako <hirako2000@gmail.com>
2017-07-29 17:47:48 +0700

    Fixes typos in doc #302
    Former-commit-id: f870334beb5cc9fb64b4dc76bd0b88e5b213a477

--- src/components/meshes/Cylinder.js
+++ src/components/meshes/Cylinder.js
@@ -108,7 +108,7 @@ class Cylinder extends MeshComponent {
 
   /**
    * @method build
-   * @description Build livecycle creates a mesh using input params.
+   * @description Build lifecycle creates a mesh using input params.
    * @param {Object} params Component parameters.
    * @return {THREE.Mesh} Built mesh
    * @memberof module:components/meshes.Cylinder
