74715bc9b3afd9d34051f3d54cfa15222bef3c85
Noah <noahcoetsee@gmail.com>
2017-10-04 10:55:12 -0400

    Added Event Handling

--- tools/networking/server.js
+++ tools/networking/server.js
@@ -7,6 +7,46 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+var objects = new Map(); // or = [] ?
+
 io.on('connection', function (socket) {
+  socket.on('addMesh', onAddMesh);
+  socket.on('removeMesh' onRemoveMesh);
   
+  socket.on('newPosition', onNewPosition);
+  socket.on('newRotation', onNewRotation);
+  
+  socket.on('newMaterial', onNewMaterial);
+  socket.on('newGeometry', onNewGeometry);
+  
+  // Add/Remove Events
+  function onAddMesh(data) {
+    
+  }
+  
+  function onRemoveMesh(data) {
+    
+  }
+  
+  // ------------------------- //
+  
+  // Position/Rotation Events
+  function onNewPosition(data) {
+    
+  }
+  
+  function onNewRotation(data) {
+    
+  }
+  
+  // ------------------------- //
+  
+  // Material/Geometry Events
+  function onNewMaterial(data) {
+    
+  }
+  
+  function onNewGeometry(data) {
+    
+  }
 });
