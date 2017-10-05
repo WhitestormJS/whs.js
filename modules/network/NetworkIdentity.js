278317f22c2eef06fc2c7019c00d23c81a741d7e
Noah <noahcoetsee@gmail.com>
2017-10-05 10:11:36 -0400

    Add base for module

--- modules/network/NetworkIdentity.js
+++ modules/network/NetworkIdentity.js
@@ -1,1 +1,15 @@
-// TELLS IF THE NETWORK SHOULD PICK UP AND SYNC THIS
+// NetworkIdentity Module
+
+export class NetworkIdentity {
+  contructor(params) {
+    this.params = Object.assign({
+      geometry: true,
+      material: true
+    }, params);
+  }
+  
+  integrate(self) {
+    const params = self.params;
+  }
+  
+}
