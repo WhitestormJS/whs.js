1ce4bfd4a0cac6abcea1fa98487b4cb8a0109702
hirako2000 <hirako2000@gmail.com>
2018-06-28 12:28:27 +0700

    Removes incorrect function #386

--- types/modules/DefineModule.d.ts
+++ types/modules/DefineModule.d.ts
@@ -3,13 +3,7 @@ export class DefineModule {
    /**
    * @constructor Creates a Define Module.
    * @param name name of the module
-   * @param data module data
+   * @param data
    */
   constructor(name: String, data: Object);
-
-  /**
-   * Plays the given clip name
-   * @param clipName - the clip name to play
-   */
-  play(clipName: string): void;
 } 
