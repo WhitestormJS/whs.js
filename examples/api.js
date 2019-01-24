d573b0f00ba742a61dc85db19fdbdae48d32c740
Noah <noahcoder77@gmail.com>
2019-01-24 08:37:59 -0500

    Create api.js

---
+++ examples/api.js
@@ -0,0 +1,9 @@
+export function connect() {
+  if (!(window.parent && window.parent.__ExamplesAPI)) return;
+
+  const api = window.parent.__ExamplesAPI;
+  api.connected = true;
+
+  console.log(`${api.PREFIX}: connected.`);
+
+  return api;
